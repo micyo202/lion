6a350bc94bae352a876c410af092670a2effba7b
micyo202 <micyo202@163.com>
2019-04-01 11:48:28 +0800

    添加Junit测试方法

---
+++ lion-demo-sample/src/test/java/com/lion/demo/sample/alg/JunitALG.java
@@ -0,0 +1,46 @@
+package com.lion.demo.sample.alg;
+
+import org.junit.Test;
+
+/**
+ * JunitALG
+ * 二分算法
+ *
+ * @author Yanzheng
+ * @date 2019/03/29
+ * Copyright 2019 Yanzheng. All rights reserved.
+ */
+public class JunitALG {
+
+    @Test
+    public void test() {
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int value = binary(a, 9);
+        System.out.println(value);
+    }
+
+    /**
+     * 二分算法
+     *
+     * @param array 数组
+     * @param value 需要查找的值
+     * @return 对应的位置
+     */
+    private int binary(int[] array, int value) {
+        int low = 0;
+        int high = array.length - 1;
+        while (low <= high) {
+            int middle = (low + high) / 2;
+            if (value == array[middle]) {
+                return middle;
+            }
+            if (value > array[middle]) {
+                low = middle + 1;
+            }
+            if (value < array[middle]) {
+                high = middle - 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ lion-demo-sample/src/test/java/com/lion/demo/sample/lambda/JunitLambda.java
@@ -0,0 +1,40 @@
+package com.lion.demo.sample.lambda;
+
+import org.assertj.core.util.Maps;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * JunitLambda
+ * lambda表达式示例代码
+ *
+ * @author Yanzheng
+ * @date 2019/03/29
+ * Copyright 2019 Yanzheng. All rights reserved.
+ */
+public class JunitLambda {
+
+    @Test
+    public void test() {
+        // 使用 stream、parallelStream 对集合进行操作
+        List<String> strings = Arrays.asList("hello tom", "hello jerry", "hello tom", "hello tom", "hello world", "hello jerry");
+        List<Integer> integers = Arrays.asList(6, 3, 2, 4, 5, 9, 8, 1, 7);
+
+        integers.stream()
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * 10)
+                .sorted((x, y) -> x > y ? -1 : 1)
+                .forEach(System.out::println);
+
+        strings.parallelStream()
+                .flatMap(x -> Arrays.stream(x.split(" ")))
+                .map(x -> Maps.newHashMap(x, 1))
+                //.map(x -> new Tuple2(x, 1))
+                //.collect(Collectors.groupingBy(Tuple2::_1));
+                .forEach(System.out::println);
+
+    }
+
+}
