9b35b5dee4cab535356f9fd0925fe69143379ea9
micyo202 <micyo202@163.com>
2019-04-03 10:58:09 +0800

    更新大数据模块

--- lion-bigdata/src/main/java/com/lion/bigdata/controller/BigDataController.java
+++ lion-bigdata/src/main/java/com/lion/bigdata/controller/BigDataController.java
@@ -1,5 +1,6 @@
 package com.lion.bigdata.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import java.util.Map;
 import java.util.UUID;
 
 /**
- * DemoController
+ * BigDataController
  * 示例控制器
  *
  * @author Yanzheng
@@ -17,7 +18,15 @@ import java.util.UUID;
  * Copyright 2019 Yanzheng. All rights reserved.
  */
 @RestController
-public class DemoController {
+public class BigDataController {
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String hi() {
+        return "Hi, I'm BigData from port: " + port;
+    }
 
     @RequestMapping("/test")
     public Map test(@RequestParam(defaultValue = "default") String name) {
