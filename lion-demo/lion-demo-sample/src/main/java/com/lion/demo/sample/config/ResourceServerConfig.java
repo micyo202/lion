60a798dd2aa64b610207630540f057548c04a5ba
micyo202 <micyo202@163.com>
2019-04-19 17:36:49 +0800

    更新资源服务器配置

--- lion-demo/lion-demo-sample/src/main/java/com/lion/demo/sample/config/ResourceServerConfig.java
+++ lion-demo/lion-demo-sample/src/main/java/com/lion/demo/sample/config/ResourceServerConfig.java
@@ -1,5 +1,7 @@
 package com.lion.demo.sample.config;
 
+import org.apache.commons.lang.ArrayUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,8 +21,12 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    // 获取yml配置文件中不需要拦截的URL
+    @Value("#{'${pattern.permit.urls}'}")
+    String[] permitUrls;
+
     // 不进行认证，直接放行的URL
-    private static final String[] PATTERNS_URL = {
+    private static final String[] PATTERN_URLS = {
             "/actuator/**",
             "/druid/**",
 
@@ -28,20 +34,19 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
             "/resources/**",
             "/swagger-ui.html",
             "/swagger-resources/**",
-            "/v2/api-docs",
-
-            "/product/**",
-            "/gray/**",
-            "/scala/**"
+            "/v2/api-docs"
     };
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
+        // 合并不许要拦截的URL数组
+        String[] excludeUrls = (String[]) ArrayUtils.addAll(PATTERN_URLS, permitUrls);
+
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers(PATTERNS_URL).permitAll()
+                .antMatchers(excludeUrls).permitAll()
                 .anyRequest().authenticated();
     }
 
