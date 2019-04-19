bece46ceb67f69abcc23ff070dd8d9b58ed49e2c
micyo202 <micyo202@163.com>
2019-04-19 16:31:30 +0800

    更新认证服务器

--- lion-auth/src/main/java/com/lion/auth/config/WebSecurityConfig.java
+++ lion-auth/src/main/java/com/lion/auth/config/WebSecurityConfig.java
@@ -6,17 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.servlet.http.HttpServletResponse;
-
 /**
  * WebSecurityConfiguration
- * security配置
+ * 安全配置
  *
  * @author Yanzheng
  * @date 2019/04/08
--- lion-auth/src/main/java/com/lion/auth/service/impl/UserDetailsServiceImpl.java
+++ lion-auth/src/main/java/com/lion/auth/service/impl/UserDetailsServiceImpl.java
@@ -27,8 +27,7 @@ import java.util.Set;
  * Copyright 2019 Yanzheng. All rights reserved.
  */
 @Service("userDetailsService")
-public class
-UserDetailsServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private UpmsClient upmsClient;
@@ -36,13 +35,13 @@ UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        Result<User> result = upmsClient.getUserByUsername(username);
+        Result<User> userResult = upmsClient.getUserByUsername(username);
 
-        if (null == result.getData()) {
+        if (null == userResult.getData()) {
             throw new UsernameNotFoundException("用户：" + username + "不存在！");
         }
 
-        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        Set<GrantedAuthority> grantedAuthorities = new HashSet();
         // 可用性 :true:可用 false:不可用
         boolean enabled = true;
         // 过期性 :true:没过期 false:过期
@@ -52,25 +51,22 @@ UserDetailsServiceImpl implements UserDetailsService {
         // 锁定性 :true:未锁定 false:已锁定
         boolean accountNonLocked = true;
 
-        User user = result.getData();
+        User user = userResult.getData();
+        // 获取用户角色
         Result<List<Role>> roleResult = upmsClient.getRoleByUserId(user.getId());
 
         if (roleResult.getCode() == Status.SUCCESS.getCode()) { // 判断获取权限列表是否成功
-            List<Role> roleList = roleResult.getData();
-            for (Role role : roleList) {
+
+            roleResult.getData().stream().forEach(role -> {
                 //角色必须是ROLE_开头，可以在数据库中设置
-                GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("ROLE_" + role.getValue().toUpperCase());
-                grantedAuthorities.add(grantedAuthority);
-                //获取权限
-                Result<List<Menu>> perResult = upmsClient.getMenuByRoleId(role.getId());
-                if (perResult.getCode() == Status.SUCCESS.getCode()) {  // 判断获取菜单列表是否成功
-                    List<Menu> menuList = perResult.getData();
-                    for (Menu menu : menuList) {
-                        GrantedAuthority authority = new SimpleGrantedAuthority(menu.getCode());
-                        grantedAuthorities.add(authority);
-                    }
+                grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role.getValue().toUpperCase()));
+                //获取菜单列表
+                Result<List<Menu>> menuResult = upmsClient.getMenuByRoleId(role.getId());
+                if (menuResult.getCode() == Status.SUCCESS.getCode()) {  // 判断获取菜单列表是否成功
+                    menuResult.getData().stream().forEach(menu -> grantedAuthorities.add(new SimpleGrantedAuthority(menu.getCode())));
                 }
-            }
+            });
+
         }
 
         org.springframework.security.core.userdetails.User securityUser = new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
