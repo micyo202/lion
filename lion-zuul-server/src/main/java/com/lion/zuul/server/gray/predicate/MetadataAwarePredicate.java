0ea95781a1805b11f4d1e48dbd8d327413bdc316
micyo202 <micyo202@163.com>
2019-04-24 10:40:46 +0800

    删除不必要的模块

---
+++ lion-zuul-server/src/main/java/com/lion/zuul/server/gray/predicate/MetadataAwarePredicate.java
@@ -0,0 +1,22 @@
+package com.lion.zuul.server.gray.predicate;
+
+import com.lion.zuul.server.gray.api.RibbonFilterContext;
+import com.lion.zuul.server.gray.support.RibbonFilterContextHolder;
+import org.springframework.cloud.alibaba.nacos.ribbon.NacosServer;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+public class MetadataAwarePredicate extends DiscoveryEnabledPredicate {
+
+    @Override
+    protected boolean apply(NacosServer server) {
+
+        final RibbonFilterContext context = RibbonFilterContextHolder.getCurrentContext();
+        final Set<Map.Entry<String, String>> attributes = Collections.unmodifiableSet(context.getAttributes().entrySet());
+        final Map<String, String> metadata = server.getMetadata();
+        return metadata.entrySet().containsAll(attributes);
+    }
+
+}
