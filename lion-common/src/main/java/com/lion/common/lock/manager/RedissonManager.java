545c7179836bb3702ee5d2d3fb81e3197d8cea6d
micyo202 <micyo202@163.com>
2019-05-14 11:15:54 +0800

    添加分布式锁管理类

---
+++ lion-common/src/main/java/com/lion/common/lock/manager/RedissonManager.java
@@ -0,0 +1,34 @@
+package com.lion.common.lock.manager;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * RedissonManager
+ * 分布式锁Redis初始化
+ *
+ * @author Yanzheng
+ * @date 2019/05/08
+ * Copyright 2019 Yanzheng. All rights reserved.
+ */
+@Configuration
+public class RedissonManager {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private String port;
+
+    @Bean
+    public RedissonClient getRedisson() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + host + ":" + port);
+        return Redisson.create(config);
+    }
+
+}
