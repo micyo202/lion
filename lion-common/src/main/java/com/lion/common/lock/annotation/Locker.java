196881cda837e900c4cb5d799d8b084b63a988d2
micyo202 <micyo202@163.com>
2019-05-14 11:16:45 +0800

    添加分布式锁注解方法

---
+++ lion-common/src/main/java/com/lion/common/lock/annotation/Locker.java
@@ -0,0 +1,22 @@
+package com.lion.common.lock.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Locker
+ * 自定义分布式锁注解
+ *
+ * @author Yanzheng
+ * @date 2019/05/08
+ * Copyright 2019 Yanzheng. All rights reserved.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Locker {
+
+    long waitTime() default 5000;   // 等待时间
+
+    long leaseTime() default 10000; // 单笔业务锁持有时长，防止死锁
+
+}
