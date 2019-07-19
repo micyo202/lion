fa55630f7fd8622c56a96034b0f744c9e2c628c8
micyo202 <micyo202@163.com>
2019-07-19 11:20:18 +0800

    更新common模块对象

--- lion-common/src/main/java/com/lion/common/entity/Result.java
+++ lion-common/src/main/java/com/lion/common/entity/Result.java
@@ -2,7 +2,6 @@ package com.lion.common.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.lion.common.util.Status;
 
 import java.io.Serializable;
 import java.util.HashMap;
