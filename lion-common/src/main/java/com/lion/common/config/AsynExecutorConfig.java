21b19ba83dbe103b2c398531e71b1218b926001b
micyo202 <micyo202@163.com>
2019-07-19 11:19:37 +0800

    更新异步线程池设置

--- lion-common/src/main/java/com/lion/common/config/AsynExecutorConfig.java
+++ lion-common/src/main/java/com/lion/common/config/AsynExecutorConfig.java
@@ -23,12 +23,12 @@ public class AsynExecutorConfig {
     @Bean("asynExecutor")
     public Executor AsynExecutor() {
         /**
-         * 核心线程数10：线程池创建时候初始化的线程数
-         * 最大线程数20：线程池最大的线程数，只有在缓冲队列满了之后才会申请超过核心线程数的线程
-         * 缓冲队列200：用来缓冲执行任务的队列
-         * 允许线程的空闲时间60秒：当超过了核心线程出之外的线程在空闲时间到达之后会被销毁
-         * 线程池名的前缀：设置好了之后可以方便我们定位处理任务所在的线程池
-         * 线程池对拒绝任务的处理策略：这里采用了CallerRunsPolicy策略，当线程池没有处理能力的时候，该策略会直接在 execute 方法的调用线程中运行被拒绝的任务；如果执行程序已关闭，则会丢弃该任务
+         * setCorePoolSize核心线程数10：线程池创建时候初始化的线程数
+         * setMaxPoolSize最大线程数20：线程池最大的线程数，只有在缓冲队列满了之后才会申请超过核心线程数的线程
+         * setQueueCapacity缓冲队列200：用来缓冲执行任务的队列
+         * setKeepAliveSeconds允许线程的空闲时间60秒：当超过了核心线程出之外的线程在空闲时间到达之后会被销毁
+         * setThreadNamePrefix线程池名的前缀：设置好了之后可以方便我们定位处理任务所在的线程池
+         * setRejectedExecutionHandler线程池对拒绝任务的处理策略：这里采用了CallerRunsPolicy策略，当线程池没有处理能力的时候，该策略会直接在 execute 方法的调用线程中运行被拒绝的任务；如果执行程序已关闭，则会丢弃该任务
          */
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(10);
@@ -37,6 +37,8 @@ public class AsynExecutorConfig {
         executor.setKeepAliveSeconds(60);
         executor.setThreadNamePrefix("AsynTaskExecutor-");
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        // 执行初始化
+        executor.initialize();
         return executor;
     }
 
