ac441f02939561efe5d97d5e8a1db744160b072d
micyo202 <micyo202@163.com>
2019-07-22 09:11:39 +0800

    添加通用异常处理类

---
+++ lion-common/src/main/java/com/lion/common/exception/LionException.java
@@ -0,0 +1,32 @@
+package com.lion.common.exception;
+
+/**
+ * LionException
+ * TODO
+ *
+ * @author Yanzheng
+ * @date 2019/07/17
+ * Copyright 2019 Yanzheng. All rights reserved.
+ */
+public class LionException extends RuntimeException {
+
+    // 错误码
+    private int code;
+
+    public LionException() {
+    }
+
+    public LionException(int code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+}
