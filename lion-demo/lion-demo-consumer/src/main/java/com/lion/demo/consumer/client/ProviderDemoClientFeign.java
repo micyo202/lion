8b28a3118869705004b15f2c8f4ca0a3977064d2
micyo202 <micyo202@163.com>
2019-08-23 17:51:45 +0800

    更新服务熔断示例代码

--- lion-demo/lion-demo-consumer/src/main/java/com/lion/demo/consumer/client/ProviderDemoClientFeign.java
+++ lion-demo/lion-demo-consumer/src/main/java/com/lion/demo/consumer/client/ProviderDemoClientFeign.java
@@ -1,19 +1,19 @@
 package com.lion.demo.consumer.client;
 
-import com.lion.demo.consumer.client.fallback.ProviderDemoClientFeignHystrix;
+import com.lion.demo.consumer.client.fallback.ProviderDemoClientFeignFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * ProviderDemoClientFeign
- * TODO
+ * 服务熔断处理
  *
- * @author Yanzheng 严正
+ * @author Yanzheng https://github.com/micyo202
  * @date 2019/01/05
  * Copyright 2019 Yanzheng. All rights reserved.
  */
-@FeignClient(value = "lion-demo-provider", fallback = ProviderDemoClientFeignHystrix.class)
+@FeignClient(value = "lion-demo-provider", fallback = ProviderDemoClientFeignFallback.class)
 public interface ProviderDemoClientFeign {
 
     @RequestMapping(value = "/sayHi")
