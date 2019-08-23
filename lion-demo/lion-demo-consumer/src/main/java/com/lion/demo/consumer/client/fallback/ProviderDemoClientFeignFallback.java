76bf27cfd2ea497a5abf055dee5d5a69f8a319f1
micyo202 <micyo202@163.com>
2019-08-23 17:51:45 +0800

    更新服务熔断示例代码

--- lion-demo/lion-demo-consumer/src/main/java/com/lion/demo/consumer/client/fallback/ProviderDemoClientFeignFallback.java
+++ lion-demo/lion-demo-consumer/src/main/java/com/lion/demo/consumer/client/fallback/ProviderDemoClientFeignFallback.java
@@ -4,17 +4,17 @@ import com.lion.demo.consumer.client.ProviderDemoClientFeign;
 import org.springframework.stereotype.Component;
 
 /**
- * ProviderDemoClientFeignHystrix
- * TODO
+ * ProviderDemoClientFeignFallback
+ * 服务熔断处理
  *
- * @author Yanzheng 严正
+ * @author Yanzheng https://github.com/micyo202
  * @date 2019/01/05
  * Copyright 2019 Yanzheng. All rights reserved.
  */
 @Component
-public class ProviderDemoClientFeignHystrix implements ProviderDemoClientFeign {
+public class ProviderDemoClientFeignFallback implements ProviderDemoClientFeign {
     @Override
     public String sayHiFromProvider(String name) {
-        return "Sorry '" + name + "', fallback hystrix!";
+        return "Sorry '" + name + "', fallback sentinel!";
     }
 }
