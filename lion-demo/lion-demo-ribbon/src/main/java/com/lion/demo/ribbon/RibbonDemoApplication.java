296d0a0bf4451c5df32a3ab49300b6044d99fe02
micyo202 <micyo202@163.com>
2019-08-24 09:25:59 +0800

    更新ribbon示例代码

--- lion-demo/lion-demo-ribbon/src/main/java/com/lion/demo/ribbon/RibbonDemoApplication.java
+++ lion-demo/lion-demo-ribbon/src/main/java/com/lion/demo/ribbon/RibbonDemoApplication.java
@@ -1,24 +1,23 @@
 package com.lion.demo.ribbon;
 
+import com.alibaba.cloud.sentinel.annotation.SentinelRestTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * RibbonDemoApplication
- * RestTemplate + Ribbon + Hystrix 示例代码
+ * RestTemplate + Ribbon + Sentinel 示例代码
  *
- * @author Yanzheng 严正
+ * @author Yanzheng https://github.com/micyo202
  * @date 2019/01/03
  * Copyright 2019 Yanzheng. All rights reserved.
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableHystrix
 public class RibbonDemoApplication {
 
     public static void main(String[] args) {
@@ -27,6 +26,7 @@ public class RibbonDemoApplication {
 
     @Bean
     @LoadBalanced
+    @SentinelRestTemplate
     RestTemplate restTemplate() {
         return new RestTemplate();
     }
