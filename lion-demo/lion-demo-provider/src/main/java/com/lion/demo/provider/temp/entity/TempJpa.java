bfbd55b7a4bb6217a122b9cd97fa1d223b8b22b5
micyo202 <micyo202@163.com>
2019-08-25 09:54:33 +0800

    更新新服务提供者示例代码

--- lion-demo/lion-demo-provider/src/main/java/com/lion/demo/provider/temp/entity/TempJpa.java
+++ lion-demo/lion-demo-provider/src/main/java/com/lion/demo/provider/temp/entity/TempJpa.java
@@ -15,7 +15,7 @@ import java.util.Date;
  * TempJpa
  * TODO
  *
- * @author Yanzheng
+ * @author Yanzheng https://github.com/micyo202
  * @date 2019/04/15
  * Copyright 2019 Yanzheng. All rights reserved.
  */
--- lion-demo/lion-demo-provider/src/main/java/com/lion/demo/provider/temp/mapper/TempMybatisCustomMapper.java
+++ lion-demo/lion-demo-provider/src/main/java/com/lion/demo/provider/temp/mapper/TempMybatisCustomMapper.java
@@ -9,7 +9,7 @@ import java.util.List;
  * TempMybatisCustomMapper
  * TODO
  *
- * @author Yanzheng
+ * @author Yanzheng https://github.com/micyo202
  * @date 2019/04/15
  * Copyright 2019 Yanzheng. All rights reserved.
  */
