61b3ba8d8255af68d69f3475b97c5414ed15aec1
micyo202 <micyo202@163.com>
2019-08-26 15:40:08 +0800

    代码优化

--- lion-demo/lion-demo-provider/src/main/java/com/lion/demo/provider/ProviderDemoApplication.java
+++ lion-demo/lion-demo-provider/src/main/java/com/lion/demo/provider/ProviderDemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * ProviderDemoApplication
  * 服务提供者demo
  *
- * @author Yanzheng 严正
+ * @author Yanzheng https://github.com/micyo202
  * @date 2019/01/05
  * Copyright 2019 Yanzheng. All rights reserved.
  */
