4201ebf91490c829a24d8e3ad4b91f41e990b5ae
micyo202 <micyo202@163.com>
2019-09-02 17:13:56 +0800

    更新scala示例代码

--- lion-demo/lion-demo-sample/src/main/scala/com/lion/demo/sample/temp/controller/SampleScalaTempController.scala
+++ lion-demo/lion-demo-sample/src/main/scala/com/lion/demo/sample/temp/controller/SampleScalaTempController.scala
@@ -6,6 +6,14 @@ import com.lion.demo.sample.temp.mapper.SampleScalaMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.{RequestMapping, RestController}
 
+/**
+  * SampleScalaTempController
+  * Scala示例控制器
+  *
+  * @author Yanzheng https://github.com/micyo202
+  * @date 2019/09/02
+  * Copyright 2019 Yanzheng. All rights reserved.
+  */
 @RestController
 @RequestMapping(Array("/scala/temp"))
 class SampleScalaTempController {
--- lion-demo/lion-demo-sample/src/main/scala/com/lion/demo/sample/temp/mapper/SampleScalaMapper.scala
+++ lion-demo/lion-demo-sample/src/main/scala/com/lion/demo/sample/temp/mapper/SampleScalaMapper.scala
@@ -2,6 +2,14 @@ package com.lion.demo.sample.temp.mapper
 
 import org.apache.ibatis.annotations.Select
 
+/**
+  * SampleScalaMapper
+  * Scala示例Mapper接口类
+  *
+  * @author Yanzheng https://github.com/micyo202
+  * @date 2019/09/02
+  * Copyright 2019 Yanzheng. All rights reserved.
+  */
 trait SampleScalaMapper {
 
   @Select(Array("SELECT * FROM temp_mybatis"))
