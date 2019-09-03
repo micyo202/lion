cc4dcf0d3750200afa13fd092f079b795d0e36d2
micyo202 <micyo202@163.com>
2019-09-03 14:32:43 +0800

    更新相关过滤器

--- lion-zuul-server/src/main/java/com/lion/zuul/server/filter/PostFilter.java
+++ lion-zuul-server/src/main/java/com/lion/zuul/server/filter/PostFilter.java
@@ -1,5 +1,6 @@
 package com.lion.zuul.server.filter;
 
+import com.lion.zuul.server.constant.FilterConstants;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
@@ -7,13 +8,12 @@ import com.netflix.zuul.exception.ZuulException;
 import javax.servlet.http.HttpServletResponse;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.POST_TYPE;
-import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SEND_RESPONSE_FILTER_ORDER;
 
 /**
  * PostFilter
- * 后置拦截器
+ * 后置拦截器（打开 @Component 注解生效）
  *
- * @author Yanzheng 严正
+ * @author Yanzheng https://github.com/micyo202
  * @date 2019/01/02
  * Copyright 2019 Yanzheng. All rights reserved.
  */
@@ -21,16 +21,28 @@ import static org.springframework.cloud.netflix.zuul.filters.support.FilterConst
 public class PostFilter extends ZuulFilter {
     @Override
     public String filterType() {
+        /**
+         * pre：路由之前
+         * routing：路由之时
+         * post： 路由之后
+         * error：发送错误调用
+         */
         return POST_TYPE;
     }
 
     @Override
     public int filterOrder() {
-        return SEND_RESPONSE_FILTER_ORDER - 1;
+        /**
+         * 过滤器执行顺序，数字越小，优先级越高，越靠前
+         */
+        return FilterConstants.POST_FILTER_ORDER - 10;
     }
 
     @Override
     public boolean shouldFilter() {
+        /**
+         * 返回 true，拦截所有 URL
+         */
         return true;
     }
 
--- lion-zuul-server/src/main/java/com/lion/zuul/server/filter/TokenFilter.java
+++ lion-zuul-server/src/main/java/com/lion/zuul/server/filter/TokenFilter.java
@@ -2,6 +2,7 @@ package com.lion.zuul.server.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lion.common.entity.Result;
+import com.lion.zuul.server.constant.FilterConstants;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
@@ -11,14 +12,13 @@ import org.springframework.http.HttpStatus;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
-import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
 
 /**
  * TokenFilter
- * token拦截器，检查token是否存在
+ * token拦截器，检查token是否存在（打开 @Component 注解生效）
  *
- * @author Yanzheng
+ * @author Yanzheng https://github.com/micyo202
  * @date 2019/04/26
  * Copyright 2019 Yanzheng. All rights reserved.
  */
@@ -26,17 +26,29 @@ import static org.springframework.cloud.netflix.zuul.filters.support.FilterConst
 public class TokenFilter extends ZuulFilter {
     @Override
     public String filterType() {
+        /**
+         * pre：路由之前
+         * routing：路由之时
+         * post： 路由之后
+         * error：发送错误调用
+         */
         return PRE_TYPE;
     }
 
     @Override
     public int filterOrder() {
-        return PRE_DECORATION_FILTER_ORDER - 1; // 过滤器执行顺序，数字越小，优先级越高，越靠前,这里设置为 0
+        /**
+         * 过滤器执行顺序，数字越小，优先级越高，越靠前
+         */
+        return FilterConstants.PRE_FILTER_ORDER + 10;
     }
 
     @Override
     public boolean shouldFilter() {
-        return true;    // 返回 true，拦截所有 URL
+        /**
+         * 返回 true，拦截所有 URL
+         */
+        return true;
     }
 
     @Override
@@ -44,6 +56,7 @@ public class TokenFilter extends ZuulFilter {
         RequestContext requestContext = RequestContext.getCurrentContext();
         HttpServletRequest request = requestContext.getRequest();
 
+        // token拦截器，检查token是否存在
         String token = request.getParameter("access_token");
         if (StringUtils.isEmpty(token)) {
             System.out.println("进入lion-zuul-server服务，执行TokenFilter，Token为空！");
