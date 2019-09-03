a4a1038a440f67910b01760888b84c25b9c00ad1
micyo202 <micyo202@163.com>
2019-09-03 14:39:23 +0800

    更新zuul路由模块

--- lion-zuul-server/src/main/java/com/lion/zuul/server/ZuulServerApplication.java
+++ lion-zuul-server/src/main/java/com/lion/zuul/server/ZuulServerApplication.java
@@ -4,21 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * ZuulServerApplication
  * 路由网管服务
  *
- * @author Yanzheng 严正
+ * @author Yanzheng https://github.com/micyo202
  * @date 2019/01/01
  * Copyright 2019 Yanzheng. All rights reserved.
  */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
-@EnableHystrix
 @EnableOAuth2Sso
 public class ZuulServerApplication {
 
