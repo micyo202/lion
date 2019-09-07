6214a92356e54851f6dff5129617be687007535b
micyo202 <micyo202@163.com>
2019-09-07 10:24:24 +0800

    添加检查 token 有效性功能

--- lion-zuul-server/src/main/java/com/lion/zuul/server/filter/TokenFilter.java
+++ lion-zuul-server/src/main/java/com/lion/zuul/server/filter/TokenFilter.java
@@ -2,12 +2,16 @@ package com.lion.zuul.server.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lion.common.entity.Result;
-import com.lion.zuul.server.constant.FilterConstants;
+import com.lion.zuul.server.constant.FilterOrderConstants;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -22,8 +26,16 @@ import static org.springframework.cloud.netflix.zuul.filters.support.FilterConst
  * @date 2019/04/26
  * Copyright 2019 Yanzheng. All rights reserved.
  */
-//@Component
+@Component
+@Slf4j
 public class TokenFilter extends ZuulFilter {
+
+    private static final String OAUTH_URL = "/oauth/";
+    private static final String ACCESS_TOKEN = "access_token";
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public String filterType() {
         /**
@@ -40,7 +52,7 @@ public class TokenFilter extends ZuulFilter {
         /**
          * 过滤器执行顺序，数字越小，优先级越高，越靠前
          */
-        return FilterConstants.PRE_FILTER_ORDER + 10;
+        return FilterOrderConstants.PRE_FILTER_ORDER + 10;
     }
 
     @Override
@@ -56,16 +68,22 @@ public class TokenFilter extends ZuulFilter {
         RequestContext requestContext = RequestContext.getCurrentContext();
         HttpServletRequest request = requestContext.getRequest();
 
-        // token拦截器，检查token是否存在
-        String token = request.getParameter("access_token");
-        if (StringUtils.isEmpty(token)) {
-            System.out.println("进入lion-zuul-server服务，执行TokenFilter，Token为空！");
+        // 获取当前请求url，若为 oauth/token 则不检查 access_token
+        String requestUrl = request.getRequestURI();
+        if (requestUrl.contains(OAUTH_URL)) {
+            return null;
+        }
+
+        // 从请求头信息获取 access_token 进行检查
+        String accessToken = request.getParameter(ACCESS_TOKEN);
+        if (StringUtils.isEmpty(accessToken)) {
+            log.info("进入 lion-gateway-server 服务，执行 TokenFilter 过滤器，access_token 为空！");
             requestContext.setSendZuulResponse(false);
             requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
 
             try {
                 ObjectMapper objectMapper = new ObjectMapper();
-                String jsonString = objectMapper.writeValueAsString(Result.failure(401, "token为空，无权访问！"));
+                String jsonString = objectMapper.writeValueAsString(Result.failure(401, "access_token 为空，无权访问！"));
                 requestContext.getResponse().setContentType("application/json;charset=UTF-8");
                 requestContext.setResponseBody(jsonString);
             } catch (IOException e) {
@@ -73,7 +91,37 @@ public class TokenFilter extends ZuulFilter {
             }
             return null;
         }
-        System.out.println("进入lion-zuul-server服务，执行TokenFilter，Token is OK!");
+
+        // 检查 access_token 的有效性
+        final String formatKey = String.format("access:%s", accessToken);
+        final Boolean hasKey = stringRedisTemplate.hasKey(formatKey);
+        if (!hasKey) {
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                String jsonString = objectMapper.writeValueAsString(Result.failure(401, "access_token 不存在，无权访问！"));
+                requestContext.getResponse().setContentType("application/json;charset=UTF-8");
+                requestContext.setResponseBody(jsonString);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        final Long expire = stringRedisTemplate.getExpire(formatKey);
+        if (0 >= expire) {
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                String jsonString = objectMapper.writeValueAsString(Result.failure(401, "access_token 已失效，无权访问！"));
+                requestContext.getResponse().setContentType("application/json;charset=UTF-8");
+                requestContext.setResponseBody(jsonString);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        log.info("进入 lion-gateway-server 服务，执行 TokenFilter 过滤器，access_token 检查成功!");
         return null;
     }
+
 }
