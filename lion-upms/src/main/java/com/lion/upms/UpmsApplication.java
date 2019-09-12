f142c895773f87c227ef62ac450e3c695f1e9a23
micyo202 <micyo202@163.com>
2019-09-12 17:54:59 +0800

    添加redis缓存策略

--- lion-upms/src/main/java/com/lion/upms/UpmsApplication.java
+++ lion-upms/src/main/java/com/lion/upms/UpmsApplication.java
@@ -1,9 +1,18 @@
 package com.lion.upms;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
 
 /**
  * UpmsApplication
@@ -16,10 +25,23 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.lion.upms..*.mapper")
+@EnableCaching
 public class UpmsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UpmsApplication.class, args);
+    }
+
+    /**
+     * 配置 redis 缓存失效时间
+     */
+    @Bean
+    public CacheManager cacheManager(@Autowired RedisConnectionFactory connectionFactory) {
+        return RedisCacheManager
+                .builder(connectionFactory)
+                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)))
+                .transactionAware()
+                .build();
     }
 
 }
