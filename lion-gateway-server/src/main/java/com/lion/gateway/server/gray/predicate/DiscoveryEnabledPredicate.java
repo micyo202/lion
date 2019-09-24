018975cd0e7619b4f56fcf6aacba2a4d763b0977
micyo202 <micyo202@163.com>
2019-09-24 09:49:56 +0800

    添加灰度相关方法

---
+++ lion-gateway-server/src/main/java/com/lion/gateway/server/gray/predicate/DiscoveryEnabledPredicate.java
@@ -0,0 +1,27 @@
+package com.lion.gateway.server.gray.predicate;
+
+import com.alibaba.cloud.nacos.ribbon.NacosServer;
+import com.netflix.loadbalancer.AbstractServerPredicate;
+import com.netflix.loadbalancer.PredicateKey;
+import org.springframework.lang.Nullable;
+
+/**
+ * DiscoveryEnabledPredicate
+ * 灰度发现启用断言抽象类
+ *
+ * @author Yanzheng https://github.com/micyo202
+ * @date 2019/09/09
+ * Copyright 2019 Yanzheng. All rights reserved.
+ */
+public abstract class DiscoveryEnabledPredicate extends AbstractServerPredicate {
+
+    @Override
+    public boolean apply(@Nullable PredicateKey input) {
+        return input != null
+                && input.getServer() instanceof NacosServer
+                && apply((NacosServer) input.getServer());
+    }
+
+    protected abstract boolean apply(NacosServer server);
+
+}
