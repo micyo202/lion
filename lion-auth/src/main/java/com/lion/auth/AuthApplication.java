a04852cb4a141bf03b7191195c2759de646d6610
micyo202 <micyo202@163.com>
2019-10-12 17:38:02 +0800

    更新认证服务启动类

--- lion-auth/src/main/java/com/lion/auth/AuthApplication.java
+++ lion-auth/src/main/java/com/lion/auth/AuthApplication.java
@@ -1,21 +1,25 @@
 package com.lion.auth;
 
+import com.lion.common.config.ScheduleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 /**
  * AuthApplication
- * TODO
+ * OAuth2用户认证服务
  *
  * @author Yanzheng https://github.com/micyo202
- * @date 2019/04/08
+ * @date 2019/09/26
  * Copyright 2019 Yanzheng. All rights reserved.
  */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients("com.lion.auth.client")
+@ComponentScan(basePackages = {"com.lion.common", "com.lion.auth"}, excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {ScheduleConfig.class})})
 public class AuthApplication {
 
     public static void main(String[] args) {
