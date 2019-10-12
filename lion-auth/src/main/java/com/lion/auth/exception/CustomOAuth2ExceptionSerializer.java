0e74b0a596e51a056170a56b4948f566b5821768
micyo202 <micyo202@163.com>
2019-10-12 17:08:41 +0800

    添加认证异常处理

---
+++ lion-auth/src/main/java/com/lion/auth/exception/CustomOAuth2ExceptionSerializer.java
@@ -0,0 +1,43 @@
+package com.lion.auth.exception;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Map;
+
+/**
+ * CustomOAuth2ExceptionSerializer
+ * 自定义认证异常序列化类
+ *
+ * @author Yanzheng
+ * @date 2019/09/27
+ * Copyright 2019 Yanzheng. All rights reserved.
+ */
+public class CustomOAuth2ExceptionSerializer extends StdSerializer<CustomOAuth2Exception> {
+
+    public CustomOAuth2ExceptionSerializer() {
+        super(CustomOAuth2Exception.class);
+    }
+
+    @Override
+    public void serialize(CustomOAuth2Exception value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        // HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        gen.writeStartObject();
+        gen.writeNumberField("code", value.getHttpErrorCode());
+        gen.writeStringField("msg", value.getMessage());
+        gen.writeNumberField("timestamp", LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli());
+        if (value.getAdditionalInformation() != null) {
+            for (Map.Entry<String, String> entry : value.getAdditionalInformation().entrySet()) {
+                String key = entry.getKey();
+                String add = entry.getValue();
+                gen.writeStringField(key, add);
+            }
+        }
+        gen.writeEndObject();
+    }
+
+}
