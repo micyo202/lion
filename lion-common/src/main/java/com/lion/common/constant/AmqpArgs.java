6136cf4b35880a216e8a4ad1f1c64ad808e714de
micyo202 <micyo202@163.com>
2019-10-12 17:13:34 +0800

    更新AMQP实现方法

---
+++ lion-common/src/main/java/com/lion/common/constant/AmqpArgs.java
@@ -0,0 +1,17 @@
+package com.lion.common.constant;
+
+/**
+ * AmqpArgs
+ * RabbitMQ 参数值
+ *
+ * @author Yanzheng
+ * @date 2019/10/12
+ * Copyright 2019 Yanzheng. All rights reserved.
+ */
+public interface AmqpArgs {
+
+    String DIRECT_QUEUE = "lionDirect";
+    String DIRECT_EXCHANGE = "lion.direct";
+    String DIRECT_ROUTING = "lion.direct.routing";
+
+}
