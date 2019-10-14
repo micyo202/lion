54a90736e0a42113ae83f94fecaf4fa92c607cbb
micyo202 <micyo202@163.com>
2019-10-14 09:26:21 +0800

    更新Jpa示例代码

--- lion-demo/lion-demo-provider/src/main/java/com/lion/demo/provider/temp/controller/TempJpaController.java
+++ lion-demo/lion-demo-provider/src/main/java/com/lion/demo/provider/temp/controller/TempJpaController.java
@@ -8,9 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -25,16 +25,18 @@ import java.util.UUID;
  */
 @Api("Jpa相关示例代码类说明文档")
 @RestController
+@RequestMapping("/temp/jpa")
 public class TempJpaController {
 
     @Autowired
     private TempRepository tempRepository;
 
     @ApiOperation(value = "Jpa方式插入数据，带事物", notes = "该方法若要触发事物回滚，插入条数大于5即可")
-    @ApiParam(name = "num", value = "插入数据条数", defaultValue = "2", required = true)
-    @RequestMapping(value = "/jpa/save/{num}", method = {RequestMethod.GET, RequestMethod.POST})
+    @ApiParam(name = "num", value = "插入数据条数", required = true)
+    @RequestMapping(value = "/save/{num}", method = {RequestMethod.GET, RequestMethod.POST})
     @Transactional
     public String jpaSave(@PathVariable int num) {
+
         if (num <= 0) {
             return "参数必须是大于0的数字";
         }
@@ -44,13 +46,13 @@ public class TempJpaController {
             String randomStr = Math.ceil(Math.random() * 100) + "";
 
             TempJpa tempJpa = new TempJpa();
-            tempJpa.setName("name-" + randomStr);
+            tempJpa.setName("TempJpaName-" + randomStr);
             tempJpa.setType(6);
             tempJpa.setStatus(true);
             tempJpa.setCreateTime(new Date());
             tempJpa.setUpdateTime(new Date());
 
-            if (i < 5) {
+            if (i <= 5) {
                 //正常插入
                 tempJpa.setId(UUID.randomUUID().toString().replaceAll("-", ""));
             } else {
@@ -67,7 +69,7 @@ public class TempJpaController {
 
     @ApiOperation(value = "Jpa方式自定义SQL查询数据", notes = "参数有效标志，必填")
     @ApiImplicitParam(name = "status", value = "有效标志", dataType = "boolean", required = true)
-    @RequestMapping(value = "/jpa/sql", method = {RequestMethod.GET, RequestMethod.POST})
+    @RequestMapping(value = "/sql", method = {RequestMethod.GET, RequestMethod.POST})
     public List<TempJpa> jpaSql(@RequestParam(defaultValue = "true") boolean status) {
         List<TempJpa> list = tempRepository.findBySql(status);
         return list;
@@ -78,7 +80,7 @@ public class TempJpaController {
             @ApiImplicitParam(name = "offset", value = "页码值", defaultValue = "0", dataType = "String"),
             @ApiImplicitParam(name = "pageSize", value = "每页条数", defaultValue = "6", dataType = "String")
     })
-    @RequestMapping(value = "/jpa/page", method = {RequestMethod.GET, RequestMethod.POST})
+    @RequestMapping(value = "/page", method = {RequestMethod.GET, RequestMethod.POST})
     public Page<TempJpa> jpaPage(@RequestParam(defaultValue = "0") int offset, @RequestParam(defaultValue = "6") int pageSize) {
         Sort sort = new Sort(Sort.Direction.DESC, "createTime");
         Pageable pageable = PageRequest.of(offset, pageSize, sort);
