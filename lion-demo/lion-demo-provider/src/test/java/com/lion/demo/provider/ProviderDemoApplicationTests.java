c6d2b8027a0ee74b118c976d3a833df0fb8ef968
micyo202 <micyo202@163.com>
2019-10-15 11:54:57 +0800

    添加Junit测试类

---
+++ lion-demo/lion-demo-provider/src/test/java/com/lion/demo/provider/ProviderDemoApplicationTests.java
@@ -0,0 +1,41 @@
+package com.lion.demo.provider;
+
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * ProviderDemoApplicationTests
+ * TODO
+ *
+ * @author Yanzheng https://github.com/micyo202
+ * @date 2019/01/06
+ * Copyright 2019 Yanzheng. All rights reserved.
+ */
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = SampleDemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProviderDemoApplicationTests {
+
+    //@LocalServerPort
+    private int port;
+
+    //@Autowired
+    private TestRestTemplate restTemplate;
+
+    //@Test
+    public void contextLoads() {
+        System.out.println("加载配置文件...");
+    }
+
+    //@Test
+    public void testRequest() {
+        String url = "http://localhost:" + port;
+        ResponseEntity<String> response1 = this.restTemplate.getForEntity(
+                url + "/gray", String.class);
+        System.out.println(String.format("/gray 调用测试结果为：%s", response1.getBody()));
+
+        ResponseEntity<String> response2 = this.restTemplate.getForEntity(
+                url + "/product/1", String.class);
+        System.out.println(String.format("/product/1 调用测试结果为：%s", response2.getBody()));
+    }
+
+}
