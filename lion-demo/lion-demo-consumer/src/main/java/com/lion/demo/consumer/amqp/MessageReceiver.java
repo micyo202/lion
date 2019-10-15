542652d87aa1e60af346ac2b3981b4ef384300d4
micyo202 <micyo202@163.com>
2019-10-15 11:50:15 +0800

    添加消息监听者

--- lion-demo/lion-demo-consumer/src/main/java/com/lion/demo/consumer/amqp/MessageReceiver.java
+++ lion-demo/lion-demo-consumer/src/main/java/com/lion/demo/consumer/amqp/MessageReceiver.java
@@ -1,25 +1,27 @@
 package com.lion.demo.consumer.amqp;
 
+import com.lion.common.constant.AmqpArgs;
+import com.lion.common.entity.Result;
+import com.lion.common.util.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 /**
- * MessageReceiver
+ * DirectReceiver
  * RabbitMQ 消息消费者（监听者）
  *
  * @author Yanzheng
- * @date 2019/09/19
+ * @date 2019/10/12
  * Copyright 2019 Yanzheng. All rights reserved.
  */
 @Component
 @Slf4j
 public class MessageReceiver {
 
-    @RabbitListener(queues = "lion")
-    public void receiver(Message message) {
-        log.info("Receiver: {}", message.getPayload());
+    @RabbitListener(queues = AmqpArgs.DIRECT_QUEUE)
+    public void receive(Result result) {
+        log.info("消息消费者接收到消息:{}", JsonUtil.jsonObj2Str(result));
     }
 
 }
