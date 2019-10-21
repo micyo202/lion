d4b0dc0b8dbe6f001064390add3622e746be283d
micyo202 <micyo202@163.com>
2019-10-21 12:01:07 +0800

    添加RAS加密工具类

---
+++ lion-common/src/main/java/com/lion/common/util/secure/RSAKey.java
@@ -0,0 +1,13 @@
+package com.lion.common.util.secure;
+
+/**
+ * RSAKey
+ * RSA公钥、私钥常量类
+ *
+ * @author Yanzheng
+ * @date 2019/10/21
+ * Copyright 2019 Yanzheng. All rights reserved.
+ */
+public enum RSAKey {
+    PUBLIC, PRIVATE
+}
