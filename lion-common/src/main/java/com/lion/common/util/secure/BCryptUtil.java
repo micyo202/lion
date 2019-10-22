70674feae413125772f1d6355c24c0cd75949ff3
micyo202 <micyo202@163.com>
2019-10-22 15:46:20 +0800

    添加BCrypt安全工具类

---
+++ lion-common/src/main/java/com/lion/common/util/secure/BCryptUtil.java
@@ -0,0 +1,39 @@
+package com.lion.common.util.secure;
+
+import jodd.crypt.BCrypt;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * BCryptUtil
+ * BCrypt工具类
+ *
+ * @author Yanzheng
+ * @date 2019/10/21
+ * Copyright 2019 Yanzheng. All rights reserved.
+ */
+public class BCryptUtil {
+
+    public static String encrypt(String password) {
+        if (StringUtils.isEmpty(password)) {
+            return null;
+        }
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean verify(String candidate, String encrypt) {
+        if (StringUtils.isEmpty(candidate) || StringUtils.isEmpty(encrypt)) {
+            return false;
+        }
+        return BCrypt.checkpw(candidate, encrypt);
+    }
+
+    public static void main(String[] args) {
+        String password = "Yanzheng -> github.com/micyo202";
+        String encrypt = BCryptUtil.encrypt(password);
+        boolean verify = BCryptUtil.verify(password, encrypt);
+        System.out.println("原文：" + password);
+        System.out.println("加密后：" + encrypt);
+        System.out.println("验证结果：" + verify);
+    }
+
+}
