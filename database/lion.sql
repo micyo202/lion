417e31a5dc8c73efa2730e0317ae9d9dada60dce
micyo202 <micyo202@163.com>
2019-10-23 17:46:47 +0800

    更新sql脚本

--- database/lion.sql
+++ database/lion.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80013
  File Encoding         : 65001
 
- Date: 09/10/2019 15:16:57
+ Date: 23/10/2019 17:43:42
 */
 
 SET NAMES utf8mb4;
@@ -22,19 +22,19 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `oauth_client_details`;
 CREATE TABLE `oauth_client_details` (
-  `client_id` varchar(48) NOT NULL,
-  `resource_ids` varchar(256) DEFAULT NULL,
-  `client_secret` varchar(256) DEFAULT NULL,
-  `scope` varchar(256) DEFAULT NULL,
-  `authorized_grant_types` varchar(256) DEFAULT NULL,
-  `web_server_redirect_uri` varchar(256) DEFAULT NULL,
-  `authorities` varchar(256) DEFAULT NULL,
+  `client_id` varchar(48) CHARACTER SET utf8 NOT NULL,
+  `resource_ids` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+  `client_secret` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+  `scope` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+  `authorized_grant_types` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+  `web_server_redirect_uri` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+  `authorities` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
   `access_token_validity` int(11) DEFAULT NULL,
   `refresh_token_validity` int(11) DEFAULT NULL,
-  `additional_information` varchar(4096) DEFAULT NULL,
-  `autoapprove` varchar(256) DEFAULT NULL,
+  `additional_information` varchar(4096) CHARACTER SET utf8 DEFAULT NULL,
+  `autoapprove` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`client_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of oauth_client_details
@@ -61,7 +61,7 @@ CREATE TABLE `sys_id` (
 -- Records of sys_id
 -- ----------------------------
 BEGIN;
-INSERT INTO `sys_id` VALUES (1, 'user_tag', '用户ID生成规则', 40, 50, '2019-04-28 16:38:40');
+INSERT INTO `sys_id` VALUES (1, 'user_tag', '用户ID生成规则', 0, 10, '2019-04-28 16:38:40');
 INSERT INTO `sys_id` VALUES (2, 'order_tag', '订单ID生成规则', 0, 1000, '2019-04-28 16:39:05');
 COMMIT;
 
@@ -71,21 +71,21 @@ COMMIT;
 DROP TABLE IF EXISTS `sys_menu`;
 CREATE TABLE `sys_menu` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `code` varchar(255) DEFAULT NULL COMMENT '菜单编码',
-  `p_code` varchar(255) DEFAULT NULL COMMENT '菜单父编码',
-  `p_id` varchar(255) DEFAULT NULL COMMENT '父菜单ID',
-  `name` varchar(255) DEFAULT NULL COMMENT '名称',
-  `url` varchar(255) DEFAULT NULL COMMENT '请求地址',
+  `code` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '菜单编码',
+  `p_code` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '菜单父编码',
+  `p_id` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '父菜单ID',
+  `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '名称',
+  `url` varchar(255) CHARACTER SET utf8 DEFAULT NULL COMMENT '请求地址',
   `is_menu` tinyint(1) DEFAULT NULL COMMENT '是否是菜单(1.菜单。2.按钮)',
   `level` int(11) DEFAULT NULL COMMENT '菜单层级',
   `sort` int(11) DEFAULT NULL COMMENT '菜单排序',
   `status` int(11) DEFAULT NULL,
-  `icon` varchar(255) DEFAULT NULL,
+  `icon` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `FK_CODE` (`code`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of sys_menu
@@ -105,16 +105,16 @@ COMMIT;
 DROP TABLE IF EXISTS `sys_role`;
 CREATE TABLE `sys_role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `value` varchar(255) DEFAULT NULL,
-  `tips` varchar(255) DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `value` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `tips` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `status` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_role_name` (`name`),
   UNIQUE KEY `unique_role_value` (`value`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of sys_role
@@ -136,7 +136,7 @@ CREATE TABLE `sys_role_menu` (
   `create_time` datetime DEFAULT NULL,
   `create_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of sys_role_menu
@@ -180,21 +180,21 @@ COMMIT;
 DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `avatar` varchar(255) DEFAULT NULL,
-  `username` varchar(45) DEFAULT NULL,
-  `password` varchar(96) DEFAULT NULL,
-  `salt` varchar(45) DEFAULT NULL,
-  `name` varchar(45) DEFAULT NULL,
+  `avatar` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `username` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+  `password` varchar(96) CHARACTER SET utf8 DEFAULT NULL,
+  `salt` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+  `name` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
   `birthday` datetime DEFAULT NULL,
   `sex` int(11) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `phone` varchar(45) DEFAULT NULL,
+  `email` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+  `phone` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
   `status` int(11) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_user_username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of sys_user
@@ -215,9 +215,9 @@ CREATE TABLE `sys_user_role` (
   `user_id` int(11) DEFAULT NULL,
   `role_id` int(11) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
-  `create_by` varchar(255) DEFAULT NULL,
+  `create_by` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Records of sys_user_role
@@ -234,14 +234,14 @@ COMMIT;
 -- ----------------------------
 DROP TABLE IF EXISTS `temp_jpa`;
 CREATE TABLE `temp_jpa` (
-  `id` varchar(32) NOT NULL,
+  `id` varchar(32) CHARACTER SET utf8 NOT NULL,
   `create_time` datetime DEFAULT NULL,
-  `name` varchar(32) DEFAULT NULL,
+  `name` varchar(32) CHARACTER SET utf8 DEFAULT NULL,
   `status` bit(1) DEFAULT NULL,
   `type` int(11) DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Table structure for temp_mybatis
@@ -252,10 +252,21 @@ CREATE TABLE `temp_mybatis` (
   `name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `type` int(11) DEFAULT NULL,
   `status` tinyint(1) DEFAULT NULL,
-  `create_time` date DEFAULT NULL,
-  `update_time` date DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+-- Table structure for temp_transactional
+-- ----------------------------
+DROP TABLE IF EXISTS `temp_transactional`;
+CREATE TABLE `temp_transactional` (
+  `id` varchar(32) COLLATE utf8_bin NOT NULL,
+  `name` varchar(36) COLLATE utf8_bin DEFAULT NULL,
+  `createTime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ----------------------------
 -- Table structure for undo_log
@@ -264,15 +275,15 @@ DROP TABLE IF EXISTS `undo_log`;
 CREATE TABLE `undo_log` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `branch_id` bigint(20) NOT NULL,
-  `xid` varchar(100) NOT NULL,
-  `context` varchar(128) NOT NULL,
+  `xid` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `context` varchar(128) CHARACTER SET utf8 NOT NULL,
   `rollback_info` longblob NOT NULL,
   `log_status` int(11) NOT NULL,
   `log_created` datetime NOT NULL,
   `log_modified` datetime NOT NULL,
-  `ext` varchar(100) DEFAULT NULL,
+  `ext` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 SET FOREIGN_KEY_CHECKS = 1;
