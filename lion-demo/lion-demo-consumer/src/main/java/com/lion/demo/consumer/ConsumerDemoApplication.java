9e071a8d9da1f8b213504853110980f716984f60
micyo202 <micyo202@163.com>
2019-10-30 13:42:48 +0800

    更新服务消费者启动类

--- lion-demo/lion-demo-consumer/src/main/java/com/lion/demo/consumer/ConsumerDemoApplication.java
+++ lion-demo/lion-demo-consumer/src/main/java/com/lion/demo/consumer/ConsumerDemoApplication.java
@@ -1,7 +1,6 @@
 package com.lion.demo.consumer;
 
 import com.alibaba.cloud.sentinel.annotation.SentinelRestTemplate;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * ConsumerDemoApplication
