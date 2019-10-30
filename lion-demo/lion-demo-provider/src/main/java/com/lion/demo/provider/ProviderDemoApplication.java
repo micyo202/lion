d9ab143c9c250286ad95457fbe3c2890f7994505
micyo202 <micyo202@163.com>
2019-10-30 13:40:32 +0800

    更新服务提供者启动类

--- lion-demo/lion-demo-provider/src/main/java/com/lion/demo/provider/ProviderDemoApplication.java
+++ lion-demo/lion-demo-provider/src/main/java/com/lion/demo/provider/ProviderDemoApplication.java
@@ -1,10 +1,10 @@
 package com.lion.demo.provider;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * ProviderDemoApplication
