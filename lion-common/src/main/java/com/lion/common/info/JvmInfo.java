fc6393425235331044d0f3f1cc059aa17ef7e384
micyo202 <micyo202@163.com>
2019-11-01 17:55:26 +0800

    添加常用信息对象类

---
+++ lion-common/src/main/java/com/lion/common/info/JvmInfo.java
@@ -0,0 +1,63 @@
+package com.lion.common.info;
+
+import java.io.Serializable;
+
+/**
+ * JvmInfo
+ * Java Virtual Machine Implementation的信息
+ *
+ * @author Yanzheng
+ * @date 2019/10/24
+ * Copyright 2019 Yanzheng. All rights reserved.
+ */
+public class JvmInfo implements Serializable {
+
+    private final String JAVA_VM_NAME = System.getProperty("java.vm.name");
+    private final String JAVA_VM_VERSION = System.getProperty("java.vm.version");
+    private final String JAVA_VM_VENDOR = System.getProperty("java.vm.vendor");
+    private final String JAVA_VM_INFO = System.getProperty("java.vm.info");
+
+    /**
+     * 取得当前JVM的名称
+     */
+    public final String getName() {
+        return JAVA_VM_NAME;
+    }
+
+    /**
+     * 取得当前JVM的版本
+     */
+    public final String getVersion() {
+        return JAVA_VM_VERSION;
+    }
+
+    /**
+     * 取得当前JVM的厂商
+     */
+    public final String getVendor() {
+        return JAVA_VM_VENDOR;
+    }
+
+    /**
+     * 取得当前JVM的信息
+     */
+    public final String getInfo() {
+        return JAVA_VM_INFO;
+    }
+
+    /**
+     * 将Java Virutal Machine Implementation的信息转换成字符串。
+     */
+    @Override
+    public final String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("JavaVM Name:    ").append(getName())
+                .append("\nJavaVM Version: ").append(getVersion())
+                .append("\nJavaVM Vendor:  ").append(getVendor())
+                .append("\nJavaVM Info:    ").append(getInfo());
+
+        return builder.toString();
+    }
+
+}
---
+++ lion-common/src/main/java/com/lion/common/info/RuntimeInfo.java
@@ -0,0 +1,65 @@
+package com.lion.common.info;
+
+import java.io.Serializable;
+
+/**
+ * RuntimeInfo
+ * 运行时信息，包括内存总大小、已用大小、可用大小等
+ *
+ * @author Yanzheng
+ * @date 2019/10/16
+ * Copyright 2019 Yanzheng. All rights reserved.
+ */
+public class RuntimeInfo implements Serializable {
+
+    private Runtime currentRuntime = Runtime.getRuntime();
+
+    /**
+     * 获得运行时对象
+     */
+    public final Runtime getRuntime() {
+        return currentRuntime;
+    }
+
+    /**
+     * 获得JVM最大可用内存
+     */
+    public final long getMaxMemory() {
+        return currentRuntime.maxMemory();
+    }
+
+    /**
+     * 获得JVM已分配内存
+     */
+    public final long getTotalMemory() {
+        return currentRuntime.totalMemory();
+    }
+
+    /**
+     * 获得JVM已分配内存中的剩余空间
+     */
+    public final long getFreeMemory() {
+        return currentRuntime.freeMemory();
+    }
+
+    /**
+     * 获得JVM最大可用内存
+     */
+    public final long getUsableMemory() {
+        return currentRuntime.maxMemory() - currentRuntime.totalMemory() + currentRuntime.freeMemory();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("Runtime:         ").append(getRuntime())
+                .append("\nMax Memory:      ").append(getMaxMemory())
+                .append("\nTotal Memory:    ").append(getTotalMemory())
+                .append("\nFree Memory:     ").append(getFreeMemory())
+                .append("\nUsable Memory:   ").append(getUsableMemory());
+
+        return builder.toString();
+    }
+
+}
