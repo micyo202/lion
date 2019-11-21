d4f17b8e9447eca34dd7d156f877e75c34ea1ec6
micyo202 <micyo202@163.com>
2019-11-21 13:24:28 +0800

    移除废弃类

--- lion-demo/lion-demo-consumer/src/main/java/com/lion/demo/consumer/temp/mapper/TempTransactionalMapper.java
+++
@@ -1,8 +0,0 @@
-package com.lion.demo.consumer.temp.mapper;
-
-import com.lion.demo.consumer.temp.model.TempTransactional;
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
-
-public interface TempTransactionalMapper extends Mapper<TempTransactional>, MySqlMapper<TempTransactional> {
-}
