1f67d4ef7a7558c5d66df38919af118bfbc7388c
micyo202 <micyo202@163.com>
2019-11-21 13:18:37 +0800

    更新定时任务Mapper类

--- lion-common/src/main/java/com/lion/common/mapper/ScheduleMapper.java
+++ lion-common/src/main/java/com/lion/common/mapper/ScheduleMapper.java
@@ -2,7 +2,6 @@ package com.lion.common.mapper;
 
 import com.lion.common.entity.Schedule;
 import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -14,7 +13,6 @@ import java.util.List;
  * @date 2019/08/09
  * Copyright 2019 Yanzheng. All rights reserved.
  */
-@Service("scheduleMapper")
 public interface ScheduleMapper {
 
     @Select("select id, name, cron, app_name appName, class_name className, method, valid from sys_schedule where valid='1' and app_name=#{appName} order by id")
