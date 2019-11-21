f03d7422fa1e3261629d9c056aa1c2a192725415
micyo202 <micyo202@163.com>
2019-11-21 13:17:11 +0800

    更新双自增Id生成服务

--- lion-id/src/main/java/com/lion/id/service/IdService.java
+++ lion-id/src/main/java/com/lion/id/service/IdService.java
@@ -1,27 +1,60 @@
 package com.lion.id.service;
 
+import com.lion.common.base.service.jpa.BaseService;
 import com.lion.id.entity.SysId;
+import com.lion.id.repository.IdRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Service;
 
 import java.util.concurrent.Future;
 
 /**
  * IdService
- * 自增Id生成服务接口（异步线程）
+ * 自增Id生成服务实现类（异步线程）
  *
  * @author Yanzheng https://github.com/micyo202
- * @date 2019/04/30
+ * @date 2019/11/21
  * Copyright 2019 Yanzheng. All rights reserved.
  */
-public interface IdService {
+@Service
+public class IdService extends BaseService<SysId, Integer> {
+
+    @Autowired
+    private IdRepository idRepository;
 
     /**
      * 同步获取SysId（用于初始化）
      */
-    SysId getSynSysId(String bizTag);
+    public SysId getSynSysId(String bizTag) {
+        return getSysId(bizTag);
+    }
 
     /**
      * 异步获取SysId（用于双buffer）
      */
-    Future<SysId> getAsynSysId(String bizTag);
+    @Async("asynExecutor")
+    public Future<SysId> getAsynSysId(String bizTag) {
+        SysId sysId = getSysId(bizTag);
+        return new AsyncResult<>(sysId);
+    }
+
+    /**
+     * 获取SysId
+     */
+    private SysId getSysId(String bizTag) {
+        SysId sysId = idRepository.getSysId(bizTag);
+
+        int increaseRange = sysId.getStep() - sysId.getMaxId();
+        int increaseCount = sysId.getStep() / (sysId.getStep() - sysId.getMaxId());
+
+        int maxId = increaseRange * increaseCount;
+        int step = increaseRange * (increaseCount + 1);
+
+        idRepository.updateSysId(maxId, step, bizTag);
+
+        return sysId;
+    }
 
 }
