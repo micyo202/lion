b040fa50409c5f67cb45b1f68efa92767535bd98
micyo202 <micyo202@163.com>
2019-11-21 13:14:01 +0800

    更新用户权限管理启动类

--- lion-upms/src/main/java/com/lion/upms/UpmsApplication.java
+++ lion-upms/src/main/java/com/lion/upms/UpmsApplication.java
@@ -1,23 +1,14 @@
 package com.lion.upms;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import tk.mybatis.spring.annotation.MapperScan;
-
-import java.time.Duration;
 
 /**
  * UpmsApplication
- * TODO
+ * 用户权限管理启动类
  *
  * @author Yanzheng https://github.com/micyo202
  * @date 2019/04/10
@@ -27,23 +18,8 @@ import java.time.Duration;
 @EnableDiscoveryClient
 @MapperScan(basePackages = {"com.lion.common.mapper", "com.lion.upms..*.mapper"})
 @ComponentScan(basePackages = {"com.lion.common", "com.lion.upms"})
-@EnableCaching
 public class UpmsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UpmsApplication.class, args);
     }
-
-    /**
-     * 配置 redis 缓存失效时间
-     */
-    @Bean
-    public CacheManager cacheManager(@Autowired RedisConnectionFactory connectionFactory) {
-        return RedisCacheManager
-                .builder(connectionFactory)
-                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)))
-                .transactionAware()
-                .build();
-    }
-
 }
