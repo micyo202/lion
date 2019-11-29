8247a8ab47f03b1f53aa5910d46d33adcff9023f
micyo202 <micyo202@163.com>
2019-11-29 09:10:18 +0800

    更新实体对象

--- lion-common/src/main/java/com/lion/common/entity/Menu.java
+++ lion-common/src/main/java/com/lion/common/entity/Menu.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 /**
  * Menu
- * 菜单实体
+ * 菜单实体类
  *
  * @author Yanzheng https://github.com/micyo202
  * @date 2019/04/13
@@ -19,22 +19,60 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Menu implements Serializable {
 
+    /**
+     * 主键
+     */
     private Integer id;
+    /**
+     * 菜单编码
+     */
     private String code;
+    /**
+     * 父菜单ID
+     */
     private Integer pId;
+    /**
+     * 菜单父编码
+     */
     private String pCode;
+    /**
+     * 菜单名称
+     */
     private String name;
+    /**
+     * 菜单url
+     */
     private String url;
-    private Boolean isMenu;
+    /**
+     * 菜单层级
+     */
     private Integer level;
+    /**
+     * 菜单排序
+     */
     private Integer sort;
+    /**
+     * 菜单图标
+     */
     private String icon;
-    private Integer status;
+    /**
+     * 是否是菜单
+     */
+    private Boolean isMenu;
+    /**
+     * 是否有效: true 有效/未删除, false 无效/已删除
+     */
+    private Boolean valid;
+    /**
+     * 创建时间
+     */
     private Date createTime;
+    /**
+     * 更新时间
+     */
     private Date updateTime;
     /**
      * 子菜单
      */
     private List<Menu> children;
-
 }
--- lion-common/src/main/java/com/lion/common/entity/User.java
+++ lion-common/src/main/java/com/lion/common/entity/User.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 /**
  * User
- * 用户实体
+ * 用户实体类
  *
  * @author Yanzheng https://github.com/micyo202
  * @date 2019/04/13
@@ -18,18 +18,68 @@ import java.util.Date;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class User implements Serializable {
 
+    /**
+     * 主键
+     */
     private Integer id;
-    private String avatar;
+    /**
+     * 用户名
+     */
     private String username;
+    /**
+     * 密码（密文）
+     */
     private String password;
-    private String salt;
+    /**
+     * 姓名
+     */
     private String name;
+    /**
+     * 生日
+     */
     private Date birthday;
-    private Integer sex;
-    private String email;
+    /**
+     * 性别: true 男, false 女
+     */
+    private Boolean sex;
+    /**
+     * 电话
+     */
     private String phone;
-    private Integer status;
+    /**
+     * 邮箱
+     */
+    private String email;
+    /**
+     * 头像
+     */
+    private String avatar;
+    /**
+     * 可用性: true 可用, false 不可用
+     */
+    private Boolean enabled;
+    /**
+     * 过期性: true 没过期, false 过期
+     */
+    private Boolean accountNonExpired;
+    /**
+     * 有效性: true 凭证有效, false 凭证无效
+     */
+    private Boolean credentialsNonExpired;
+    /**
+     * 锁定性: true 未锁定, false 已锁定
+     */
+    private Boolean accountNonLocked;
+    /**
+     * 是否有效: true 有效/未删除, false 无效/已删除
+     */
+    private Boolean valid;
+    /**
+     * 创建时间
+     */
     private Date createTime;
+    /**
+     * 更新时间
+     */
     private Date updateTime;
-
 }
