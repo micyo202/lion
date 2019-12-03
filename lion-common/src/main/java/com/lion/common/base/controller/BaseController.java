e47330612af1b8c0cd976c16399594ea9bdb6acb
micyo202 <micyo202@163.com>
2019-12-03 14:34:15 +0800

    更新基类控制器

--- lion-common/src/main/java/com/lion/common/base/controller/BaseController.java
+++ lion-common/src/main/java/com/lion/common/base/controller/BaseController.java
@@ -1,18 +1,26 @@
 package com.lion.common.base.controller;
 
 import com.lion.common.amqp.MessageSender;
-import com.lion.common.info.*;
+import com.lion.common.exception.LionException;
+import com.lion.common.util.DateUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.support.ApplicationObjectSupport;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * BaseController
@@ -23,6 +31,7 @@ import javax.servlet.http.HttpSession;
  * Copyright 2019 Yanzheng. All rights reserved.
  */
 @RestController
+@Slf4j
 public abstract class BaseController extends ApplicationObjectSupport {
 
     /**
@@ -34,7 +43,7 @@ public abstract class BaseController extends ApplicationObjectSupport {
     /**
      * 应用名称（默认：lion-unknown）
      */
-    @Value("${spring.application.name:lion-unknown}")
+    @Value("${spring.application.name}")
     protected String applicationName;
 
     /**
@@ -48,6 +57,12 @@ public abstract class BaseController extends ApplicationObjectSupport {
      */
     @Value("${security.oauth2.client.access-token-uri:http://localhost:8888/oauth/token}")
     protected String authServer;
+
+    /**
+     * 文件上传路径（默认：/tmp）
+     */
+    @Value("${file.upload.path:/tmp}")
+    protected String fileUploadPath;
 
     /**
      * RabbitMQ 消息发送者
@@ -115,25 +130,116 @@ public abstract class BaseController extends ApplicationObjectSupport {
     }
 
     /**
-     * 健康检查
+     * 文件上传（支持多个文件上传）
      */
-    @GetMapping("/health")
-    public String health() {
+    protected List<String> fileUpload(HttpServletRequest request) {
 
-        UserInfo userInfo = new UserInfo();
-        OsInfo osInfo = new OsInfo();
-        JavaInfo javaInfo = new JavaInfo();
-        JvmInfo jvmInfo = new JvmInfo();
-        RuntimeInfo runtimeInfo = new RuntimeInfo();
+        List<String> list = new ArrayList<>();
 
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("[UserInfo]\n").append(userInfo.toString()).append("\n---------")
-                .append("\n[OsInfo]\n").append(osInfo.toString()).append("\n---------")
-                .append("\n[JavaInfo]\n").append(javaInfo.toString()).append("\n---------")
-                .append("\n[JvmInfo]\n").append(jvmInfo.toString()).append("\n---------")
-                .append("\n[RuntimeInfo]\n").append(runtimeInfo.toString()).append("\n");
+        // 转换成多部分request
+        MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+        // 取得request中的所有文件名
+        Iterator<String> iterator = multiRequest.getFileNames();
+        while (iterator.hasNext()) {
+            // 取得上传文件
+            MultipartFile file = multiRequest.getFile(iterator.next());
+            if (null != file) {
+                // 获取当前上传文件的文件名称
+                String originalFileName = file.getOriginalFilename();
+                // 如果名称不为空说明该文件存在，否则说明该文件不存在
+                if (!"".equals(originalFileName)) {
+                    long timestamp = DateUtil.getTimestamp();
+                    String currentDate = DateUtil.formatLocalDateTime(DateUtil.timestampToLocalDateTime(timestamp), "yyyy/MM/dd");
+                    // 获取文件后缀
+                    String fileSuffix = originalFileName.substring(originalFileName.lastIndexOf("."));
+                    // 重命名上传的文件名（格式：时间戳.后缀）
+                    String fileName = timestamp + fileSuffix;
+                    File dest = new File(fileUploadPath + "/upload/" + applicationName + "/" + currentDate, fileName);
+                    // 判断文件父目录是否存在
+                    if (!dest.getParentFile().exists()) {
+                        dest.getParentFile().mkdirs();
+                    }
+                    try {
+                        // 保存文件成功后记录上传后的名称
+                        file.transferTo(dest);
+                        list.add(fileName);
+                    } catch (IOException e) {
+                        throw new LionException(e.getMessage());
+                    }
+                }
+            }
+        }
+        return list.isEmpty() ? null : list;
+    }
 
-        return stringBuilder.toString();
+    /**
+     * 文件下载
+     *
+     * @param fileName 通过 upload 方法上传（文件名为时间戳）
+     * @param response 响应对象
+     */
+    protected boolean fileDownload(String fileName, HttpServletResponse response) {
+
+        if (StringUtils.isEmpty(fileName)) {
+            return false;
+        }
+
+        // 获取文件名称（上传的时间戳）
+        long filePrefix = Long.valueOf(fileName.substring(0, fileName.lastIndexOf(".")));
+        String uploadDate = DateUtil.formatLocalDateTime(DateUtil.timestampToLocalDateTime(filePrefix), "yyyy/MM/dd");
+
+        //设置文件路径
+        String filePath = fileUploadPath + "/upload/" + applicationName + "/" + uploadDate;
+        File file = new File(filePath, fileName);
+
+        // 判断文件父目录是否存在
+        if (file.exists()) {
+            response.reset();
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/octet-stream");
+            //response.setHeader("Content-Type", "application/octet-stream");
+            // 设置文件名
+            response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null;
+            BufferedInputStream bis = null;
+
+            try {
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                OutputStream os = response.getOutputStream();
+                int i = bis.read(buffer);
+
+                while (i != -1) {
+                    os.write(buffer, 0, i);
+                    i = bis.read(buffer);
+                }
+
+                return true;
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+                return false;
+            } finally {
+                if (null != bis) {
+                    try {
+                        bis.close();
+                    } catch (IOException e) {
+                        log.error(e.getMessage(), e);
+                    }
+                }
+                if (null != fis) {
+                    try {
+                        fis.close();
+                    } catch (IOException e) {
+                        log.error(e.getMessage(), e);
+                    }
+                }
+            }
+
+        }
+
+        return false;
     }
 
 }
