4106e607797889cdc04604b0396838a0e5f79325
micyo202 <micyo202@163.com>
2019-12-05 17:22:55 +0800

    更新定时任务Mapper

--- lion-common/src/main/java/com/lion/common/mapper/ScheduleMapper.java
+++ lion-common/src/main/java/com/lion/common/mapper/ScheduleMapper.java
@@ -15,7 +15,7 @@ import java.util.List;
  */
 public interface ScheduleMapper {
 
-    @Select("select id, name, cron, app_name appName, class_name className, method, valid from sys_schedule where valid='1' and app_name=#{appName} order by id")
+    @Select("select id, name, cron, app_name appName, class_name className, method, valid from sys_schedule where valid=1 and app_name=#{appName} order by id")
     List<Schedule> getScheduleListByAppName(String appName);
 
 }
