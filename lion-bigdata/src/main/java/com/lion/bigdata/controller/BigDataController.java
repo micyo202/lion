3530fb509aa40c5b5f4b21dc7dccad5bc444c47e
micyo202 <micyo202@163.com>
2019-12-31 09:15:14 +0800

    更新大数据示例

--- lion-bigdata/src/main/java/com/lion/bigdata/controller/BigDataController.java
+++ lion-bigdata/src/main/java/com/lion/bigdata/controller/BigDataController.java
@@ -2,12 +2,7 @@ package com.lion.bigdata.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 /**
  * BigDataController
@@ -26,16 +21,6 @@ public class BigDataController {
     @RequestMapping("/hi")
     public String hi() {
         return "Hi, I'm BigData from port: " + port;
-    }
-
-    @RequestMapping("/test")
-    public Map test(@RequestParam(defaultValue = "default") String name) {
-        Map<String, Object> map = new HashMap<>(4, 1);
-        map.put("id", UUID.randomUUID().toString().replaceAll("-", ""));
-        map.put("name", name);
-        map.put("age", 27);
-        map.put("sex", true);
-        return map;
     }
 
 }
