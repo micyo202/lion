3e828268434970197a40e2d2b16940a4e95bbd5c
Yanzheng <micyo202@163.com>
2020-03-12 16:27:55 +0800

    更新用户认证模块控制器

--- lion-auth/src/main/java/com/lion/auth/controller/UserController.java
+++ lion-auth/src/main/java/com/lion/auth/controller/UserController.java
@@ -6,7 +6,10 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
