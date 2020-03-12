27739cadfbe2e2d985e1d4e34262af54975e939f
Yanzheng <micyo202@163.com>
2020-03-12 16:28:48 +0800

    更新认证服务启动类

--- lion-auth/src/main/java/com/lion/auth/AuthApplication.java
+++ lion-auth/src/main/java/com/lion/auth/AuthApplication.java
@@ -1,31 +1,29 @@
 package com.lion.auth;
 
+import com.lion.common.config.ResourceServerConfig;
 import com.lion.common.config.ScheduleConfig;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
 /**
  * AuthApplication
- * OAuth2用户认证服务
+ * OAuth2权限认证服务
  *
  * @author Yanzheng https://github.com/micyo202
- * @date 2019/09/26
- * Copyright 2019 Yanzheng. All rights reserved.
+ * @date 2020/2/5
+ * Copyright 2020 Yanzheng. All rights reserved.
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients("com.lion.auth.client")
+@MapperScan("com.lion.auth.mapper")
 @ComponentScan(basePackages = {"com.lion.common", "com.lion.auth"},
-        excludeFilters = {
-        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {ScheduleConfig.class})})
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {ScheduleConfig.class, ResourceServerConfig.class})})
 public class AuthApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
     }
-
 }
