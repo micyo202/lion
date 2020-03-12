bccd8cb57c4bf9c92f0c20550cccd3631b3421a4
Yanzheng <micyo202@163.com>
2020-03-12 15:30:53 +0800

    更新安全配置

---
+++ lion-admin/src/main/java/com/lion/admin/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.lion.admin.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+/**
+ * SecurityConfig
+ * 安全认证配置
+ *
+ * @author Yanzheng https://github.com/micyo202
+ * @date 2019/01/01
+ * Copyright 2019 Yanzheng. All rights reserved.
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final String adminContextPath;
+
+    public SecurityConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(adminContextPath + "/");
+
+        http.authorizeRequests()
+                .antMatchers(adminContextPath + "/instances").permitAll()
+                .antMatchers(adminContextPath + "/actuator/**").permitAll()
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler)
+                .and()
+                .logout().logoutUrl(adminContextPath + "/logout")
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+}
