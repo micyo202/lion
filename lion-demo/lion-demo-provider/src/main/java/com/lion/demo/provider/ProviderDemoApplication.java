3e8995211f7743e45e1e6d748510dc1f8dfa8eb9
Yanzheng <micyo202@163.com>
2020-03-23 14:28:47 +0800

    更新服务提供者

--- lion-demo/lion-demo-provider/src/main/java/com/lion/demo/provider/ProviderDemoApplication.java
+++ lion-demo/lion-demo-provider/src/main/java/com/lion/demo/provider/ProviderDemoApplication.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -17,6 +18,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 @EnableKnife4j
 //@EnableTransactionManagement
 //@MapperScan("com.lion.**.mapper")
