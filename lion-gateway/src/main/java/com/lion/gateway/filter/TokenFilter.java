511642f75d7bd69ca506fb4871bf5bb5ec584d38
Yanzheng <micyo202@163.com>
2020-03-25 10:18:35 +0800

    更新Token过滤器

--- lion-gateway/src/main/java/com/lion/gateway/filter/TokenFilter.java
+++ lion-gateway/src/main/java/com/lion/gateway/filter/TokenFilter.java
@@ -1,6 +1,7 @@
 package com.lion.gateway.filter;
 
 import com.lion.common.constant.ResponseStatus;
+import com.lion.common.constant.SecurityConstant;
 import com.lion.common.entity.Result;
 import com.lion.common.util.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +22,7 @@ import java.nio.charset.StandardCharsets;
 /**
  * TokenFilter
  * token 过滤器
+ * access_token需请求时放在header中，格式'Authorization: Bearer token'
  *
  * @author Yanzheng https://github.com/micyo202
  * @date 2019/09/04
@@ -29,48 +31,62 @@ import java.nio.charset.StandardCharsets;
 @Slf4j
 public class TokenFilter implements GlobalFilter, Ordered {
 
-    private static final String OAUTH_URL = "/oauth/";
-    private static final String ACCESS_TOKEN = "access_token";
-
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
     @Override
+    public int getOrder() {
+        /**
+         * 值越大，优先级越低
+         */
+        return 10;
+    }
+
+    @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
-        // 获取当前请求url，若为 oauth/token 则不检查 access_token
+        // 获取当前请求url，若为包含 /oauth/ 则不检查 access_token
         String requestUrl = exchange.getRequest().getURI().toString();
-        if (requestUrl.contains(OAUTH_URL)) {
+        if (requestUrl.contains(SecurityConstant.OAUTH_URL)) {
             return chain.filter(exchange);
         }
 
         ServerHttpResponse response = exchange.getResponse();
 
         // 从请求头信息获取 access_token 进行检查
-        String accessToken = exchange.getRequest().getQueryParams().getFirst(ACCESS_TOKEN);
+        String accessToken = exchange.getRequest().getHeaders().getFirst(SecurityConstant.ACCESS_TOKEN);
         if (StringUtils.isEmpty(accessToken)) {
             response.setStatusCode(HttpStatus.UNAUTHORIZED);
-            String jsonString = JsonUtil.jsonObj2Str(Result.failure(ResponseStatus.UNAUTHORIZED.code(), "access_token 不能为空"));
+            String jsonString = JsonUtil.jsonObj2Str(Result.failure(ResponseStatus.UNAUTHORIZED.code(), "Access Token 不能为空"));
             return getVoidMono(response, jsonString);
         }
 
+        // 判断 access_token 是否 Bearer  开头
+        if (!accessToken.startsWith(SecurityConstant.BEARER_PREFIX)) {
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            String jsonString = JsonUtil.jsonObj2Str(Result.failure(ResponseStatus.UNAUTHORIZED.code(), "Access Token 格式不正确"));
+            return getVoidMono(response, jsonString);
+        }
+
+        // 格式化 access_token 前缀 Bearer  -> access:
+        final String formatKey = accessToken.replace(SecurityConstant.BEARER_PREFIX, SecurityConstant.ACCESS_PREFIX);
+
         // 检查 access_token 的有效性
-        final String formatKey = String.format("access:%s", accessToken);
         final Boolean hasKey = stringRedisTemplate.hasKey(formatKey);
         if (!hasKey) {
             response.setStatusCode(HttpStatus.UNAUTHORIZED);
-            String jsonString = JsonUtil.jsonObj2Str(Result.failure(ResponseStatus.UNAUTHORIZED.code(), "无效的 access_token"));
+            String jsonString = JsonUtil.jsonObj2Str(Result.failure(ResponseStatus.UNAUTHORIZED.code(), "无效的 Access Token"));
             return getVoidMono(response, jsonString);
         }
 
         final Long expire = stringRedisTemplate.getExpire(formatKey);
         if (0 >= expire) {
             response.setStatusCode(HttpStatus.UNAUTHORIZED);
-            String jsonString = JsonUtil.jsonObj2Str(Result.failure(ResponseStatus.UNAUTHORIZED.code(), "access_token 已过期"));
+            String jsonString = JsonUtil.jsonObj2Str(Result.failure(ResponseStatus.UNAUTHORIZED.code(), "Access Token 已过期"));
             return getVoidMono(response, jsonString);
         }
 
-        log.info("进入 lion-gateway 服务，执行 TokenFilter 过滤器，检查 access_token 完成");
+        log.info("进入 lion-gateway 服务，执行 TokenFilter 过滤器，检查 Access Token 完成");
         return chain.filter(exchange);
 
     }
@@ -80,14 +96,6 @@ public class TokenFilter implements GlobalFilter, Ordered {
         byte[] datas = jsonString.getBytes(StandardCharsets.UTF_8);
         DataBuffer buffer = response.bufferFactory().wrap(datas);
         return response.writeWith(Mono.just(buffer));
-    }
-
-    @Override
-    public int getOrder() {
-        /**
-         * 值越大，优先级越低
-         */
-        return 10;
     }
 
 }
