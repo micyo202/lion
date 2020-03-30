7008852b4f73b028edc7c9077828e268efed914b
Yanzheng <micyo202@163.com>
2020-03-30 16:57:40 +0800

    更新seata数据源代理配置类

--- lion-demo/lion-demo-consumer/src/main/java/com/lion/demo/consumer/config/SeataDataSourceProxyConfig.java
+++ lion-demo/lion-demo-consumer/src/main/java/com/lion/demo/consumer/config/SeataDataSourceProxyConfig.java
@@ -1,44 +1,55 @@
 package com.lion.demo.consumer.config;
 
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+import javax.sql.DataSource;
+
 /**
  * SeataDataSourceProxyConfig
- * 分布式事物 Seata 数据源代理配置（Mybatis & Jpa）
+ * 分布式事务 Seata 数据源代理配置
+ * 注：1.0.0 无需手动构造数据源代理，直接使用数据源即可
  *
  * @author Yanzheng https://github.com/micyo202
  * @date 2019/09/24
  * Copyright 2019 Yanzheng. All rights reserved.
  */
-//@Configuration
+@Configuration
 public class SeataDataSourceProxyConfig {
 
-    /*
+    @Value("${mybatis-plus.mapper-locations}")
+    private String mapperLocations;
+
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactoryBean(DataSource dataSource) throws Exception {
+        MybatisSqlSessionFactoryBean bean = new MybatisSqlSessionFactoryBean();
+        bean.setDataSource(dataSource);
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        //bean.setConfigLocation(resolver.getResource("classpath:mybatis-config.xml"));
+        bean.setMapperLocations(resolver.getResources(mapperLocations));
+
+        SqlSessionFactory factory;
+        try {
+            factory = bean.getObject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return factory;
+    }
+
+    /**
+     * @param sqlSessionFactory SqlSessionFactory
+     * @return SqlSessionTemplate
+     */
     @Bean
     public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
         return new SqlSessionTemplate(sqlSessionFactory);
     }
-    */
 
-    /**
-     * 从配置文件获取属性构造datasource，注意前缀，这里用的是druid，根据自己情况配置,
-     * 原生datasource前缀取"spring.datasource"
-     */
-    /*
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.druid")
-    public DataSource dataSource() {
-        DruidDataSource druidDataSource = new DruidDataSource();
-        return druidDataSource;
-    }
-    */
-
-    /**
-     * 构造datasource代理对象，替换原来的datasource
-     */
-    /*
-    @Primary
-    @Bean("dataSource")
-    public DataSourceProxy dataSourceProxy(DataSource dataSource) {
-        return new DataSourceProxy(dataSource);
-    }
-    */
 }
