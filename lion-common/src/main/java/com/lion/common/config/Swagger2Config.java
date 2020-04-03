ba74abd1fac431f37a9add1db20587c7858acff7
Yanzheng <micyo202@163.com>
2020-04-03 21:43:35 +0800

    Upgrade to version 2.0.3

--- lion-common/src/main/java/com/lion/common/config/Swagger2Config.java
+++ lion-common/src/main/java/com/lion/common/config/Swagger2Config.java
@@ -62,7 +62,7 @@ public class Swagger2Config {
                 .termsOfServiceUrl("https://github.com/micyo202/lion")
                 //创建人
                 .contact(new Contact("Yanzheng", "https://github.com/micyo202/lion", "micyo202@163.com"))
-                .version("2.0.2")
+                .version("2.0.3")
                 .build();
     }
 
