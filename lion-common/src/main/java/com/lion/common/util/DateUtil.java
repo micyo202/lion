bed8490dd3eba2de7f930453611d4b990bed5100
Yanzheng <micyo202@163.com>
2020-05-11 11:28:37 +0800

    更新日期工具类

--- lion-common/src/main/java/com/lion/common/util/DateUtil.java
+++ lion-common/src/main/java/com/lion/common/util/DateUtil.java
@@ -17,7 +17,6 @@ package com.lion.common.util;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjusters;
 
 /**
@@ -29,38 +28,22 @@ import java.time.temporal.TemporalAdjusters;
  */
 public class DateUtil {
 
-    private static final DateTimeFormatter YEAR_MONTH_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM");
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-    private static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
-    private static final DateTimeFormatter SHORT_YEAR_MONTH_FORMATTER = DateTimeFormatter.ofPattern("yyyyMM");
-    private static final DateTimeFormatter SHORT_DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
-    private static final DateTimeFormatter SHORT_DATETIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-    private static final DateTimeFormatter SHORT_TIME_FORMATTER = DateTimeFormatter.ofPattern("HHmmss");
-
     /**
-     * 返回当前的日期
+     * 自定义格式化
      */
-    public static LocalDate getCurrentLocalDate() {
-        return LocalDate.now();
-    }
-
-    /**
-     * 返回当前时间
-     */
-    public static LocalTime getCurrentLocalTime() {
-        return LocalTime.now();
-    }
-
-    /**
-     * 返回当前日期时间
-     */
-    public static LocalDateTime getCurrentLocalDateTime() {
-        return LocalDateTime.now();
-    }
+    private static final String YEAR_MONTH_FORMATTER = "yyyy-MM";
+    private static final String DATE_FORMATTER = "yyyy-MM-dd";
+    private static final String DATETIME_FORMATTER = "yyyy-MM-dd HH:mm:ss";
+    private static final String TIME_FORMATTER = "HH:mm:ss";
+    private static final String YEAR_MONTH_FORMATTER_SHORT = "yyyyMM";
+    private static final String DATE_FORMATTER_SHORT = "yyyyMMdd";
+    private static final String DATETIME_FORMATTER_SHORT = "yyyyMMddHHmmss";
+    private static final String TIME_FORMATTER_SHORT = "HHmmss";
 
     /**
      * 获取当前时间戳
+     *
+     * @return timestamp
      */
     public static long getTimestamp() {
         // LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli();
@@ -71,101 +54,185 @@ public class DateUtil {
     }
 
     /**
-     * yyyy
+     * 获取当前年份
+     *
+     * @return yyyy
      */
     public static int getCurrentYear() {
         return LocalDate.now().getYear();
     }
 
     /**
-     * yyyy-MM
+     * 获取当前年月
+     *
+     * @return yyyy-MM
      */
-    public static String getCurrentMonth() {
-        return LocalDate.now().format(YEAR_MONTH_FORMATTER);
+    public static String getCurrentYearMonth() {
+        return getCurrentDate(YEAR_MONTH_FORMATTER);
     }
 
     /**
-     * yyyy-MM-dd
+     * 获取当前日期
+     *
+     * @return yyyy-MM-dd
      */
     public static String getCurrentDate() {
-        return LocalDate.now().format(DATE_FORMATTER);
+        return getCurrentDate(DATE_FORMATTER);
     }
 
     /**
-     * HHmmss
+     * 获取下一天日期
+     *
+     * @return yyyy-MM-dd
+     */
+    public static String getNextDate() {
+        return getNextDate(DATE_FORMATTER);
+    }
+
+    /**
+     * 获取当前时间
+     *
+     * @return HHmmss
      */
     public static String getCurrentTime() {
-        return LocalTime.now().format(TIME_FORMATTER);
+        return getCurrentTime(TIME_FORMATTER);
     }
 
     /**
-     * yyyy-MM-dd HH:mm:ss
+     * 获取当前日期时间
+     *
+     * @return yyyy-MM-dd HH:mm:ss
      */
     public static String getCurrentDateTime() {
-        return LocalDateTime.now().format(DATETIME_FORMATTER);
+        return getCurrentDateTime(DATETIME_FORMATTER);
     }
 
     /**
-     * yyyyMM
+     * 获取当前年月
+     *
+     * @return yyyyMM
      */
-    public static String getCurrentShortYearMonth() {
-        return LocalDate.now().format(SHORT_YEAR_MONTH_FORMATTER);
+    public static String getCurrentYearMonthShort() {
+        return getCurrentDate(YEAR_MONTH_FORMATTER_SHORT);
     }
 
     /**
-     * yyyyMMdd
+     * 获取当前日期
+     *
+     * @return yyyyMMdd
      */
-    public static String getCurrentShortDate() {
-        return LocalDate.now().format(SHORT_DATE_FORMATTER);
+    public static String getCurrentDateShort() {
+        return getCurrentDate(DATE_FORMATTER_SHORT);
     }
 
     /**
-     * HHmmss
+     * 获取下一天日期
+     *
+     * @return yyyy-MM-dd
      */
-    public static String getCurrentShortTime() {
-        return LocalTime.now().format(SHORT_TIME_FORMATTER);
+    public static String getNextDateShort() {
+        return getNextDate(DATE_FORMATTER_SHORT);
     }
 
     /**
-     * yyyyMMddHHmmss
+     * 获取当前时间
+     *
+     * @return HHmmss
      */
-    public static String getCurrentShortDateTime() {
-        return LocalDateTime.now().format(SHORT_DATETIME_FORMATTER);
+    public static String getCurrentTimeShort() {
+        return getCurrentTime(TIME_FORMATTER_SHORT);
     }
 
+    /**
+     * 获取当前日期时间
+     *
+     * @return yyyyMMddHHmmss
+     */
+    public static String getCurrentDateTimeShort() {
+        return getCurrentDateTime(DATETIME_FORMATTER_SHORT);
+    }
+
+    /**
+     * 获取当前日期
+     *
+     * @param pattern 格式化
+     */
     public static String getCurrentDate(String pattern) {
         return LocalDate.now().format(DateTimeFormatter.ofPattern(pattern));
     }
 
+    /**
+     * 获取下一天日期
+     *
+     * @param pattern 格式化
+     */
+    public static String getNextDate(String pattern) {
+        return LocalDate.now().plusDays(1).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    /**
+     * 获取当前时间
+     *
+     * @param pattern 格式化
+     */
     public static String getCurrentTime(String pattern) {
         return LocalTime.now().format(DateTimeFormatter.ofPattern(pattern));
     }
 
+    /**
+     * 获取当前日期时间
+     *
+     * @param pattern 格式化
+     */
     public static String getCurrentDateTime(String pattern) {
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern(pattern));
     }
 
     /**
-     * 时间戳与日期类型互转
+     * 时间戳转日期
+     *
+     * @param timestamp 时间戳
      */
     public static LocalDateTime timestampToLocalDateTime(long timestamp) {
         Instant instant = Instant.ofEpochMilli(timestamp);
         return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
     }
 
+    /**
+     * 日期转时间戳
+     *
+     * @param localDateTime 日期
+     */
     public static long localDateTimeToTimestamp(LocalDateTime localDateTime) {
         return localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
     }
 
     /**
-     * 时间戳格式化字符串
+     * 时间戳转日期时间
+     *
+     * @param timestamp 时间戳
+     * @return yyyy-MM-dd HH:mm:ss
      */
     public static String formatTimestamp(long timestamp) {
-        Instant instant = Instant.ofEpochMilli(timestamp);
-        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
-        return formatLocalDateTime(localDateTime);
+        return formatTimestamp(timestamp, DATETIME_FORMATTER);
     }
 
+    /**
+     * 时间戳转日期时间
+     *
+     * @param timestamp 时间戳
+     * @return yyyyMMddHHmmss
+     */
+    public static String formatTimestampShort(long timestamp) {
+        return formatTimestamp(timestamp, DATETIME_FORMATTER_SHORT);
+    }
+
+    /**
+     * 时间戳转日期
+     *
+     * @param timestamp 时间戳
+     * @param pattern   格式化
+     */
     public static String formatTimestamp(long timestamp, String pattern) {
         Instant instant = Instant.ofEpochMilli(timestamp);
         LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
@@ -173,55 +240,121 @@ public class DateUtil {
     }
 
     /**
-     * 日期对象转字符串
+     * 日期格式化字符串
+     *
+     * @param localDate 日期
+     * @return yyyy-MM-dd
      */
-    public static String formatLocalDate(LocalDate date) {
-        return date.format(DATE_FORMATTER);
-    }
-
-    public static String formatLocalTime(LocalTime time) {
-        return time.format(TIME_FORMATTER);
-    }
-
-    public static String formatLocalDateTime(LocalDateTime dateTime) {
-        return dateTime.format(DATETIME_FORMATTER);
-    }
-
-    public static String formatLocalDate(LocalDate date, String pattern) {
-        return date.format(DateTimeFormatter.ofPattern(pattern));
-    }
-
-    public static String formatLocalTime(LocalTime time, String pattern) {
-        return time.format(DateTimeFormatter.ofPattern(pattern));
-    }
-
-    public static String formatLocalDateTime(LocalDateTime datetime, String pattern) {
-        return datetime.format(DateTimeFormatter.ofPattern(pattern));
+    public static String formatLocalDate(LocalDate localDate) {
+        return formatLocalDate(localDate, DATE_FORMATTER);
     }
 
     /**
-     * 字符串转日期对象
+     * 日期格式化字符串
+     *
+     * @param localDate 日期
+     * @return yyyyMMdd
      */
-    public static LocalDate parseLocalDate(String date) {
-        return LocalDate.parse(date, DATE_FORMATTER);
+    public static String formatLocalDateShort(LocalDate localDate) {
+        return formatLocalDate(localDate, DATE_FORMATTER_SHORT);
     }
 
-    public static LocalTime parseLocalTime(String time) {
-        return LocalTime.parse(time, TIME_FORMATTER);
+    /**
+     * 时间格式化字符串
+     *
+     * @param localTime 时间
+     * @return HH:mm:ss
+     */
+    public static String formatLocalTime(LocalTime localTime) {
+        return formatLocalTime(localTime, TIME_FORMATTER);
     }
 
-    public static LocalDateTime parseLocalDateTime(String dateTime) {
-        return LocalDateTime.parse(dateTime, DATETIME_FORMATTER);
+    /**
+     * 时间格式化字符串
+     *
+     * @param localTime 时间
+     * @return HHmmss
+     */
+    public static String formatLocalTimeShort(LocalTime localTime) {
+        return formatLocalTime(localTime, TIME_FORMATTER_SHORT);
     }
 
+    /**
+     * 日期时间格式化字符串
+     *
+     * @param localDateTime 日期时间
+     * @return yyyy-MM-dd HH:mm:ss
+     */
+    public static String formatLocalDateTime(LocalDateTime localDateTime) {
+        return formatLocalDateTime(localDateTime, DATETIME_FORMATTER);
+    }
+
+    /**
+     * 日期时间格式化字符串
+     *
+     * @param localDateTime 日期时间
+     * @return yyyyMMddHHmmss
+     */
+    public static String formatLocalDateTimeShort(LocalDateTime localDateTime) {
+        return formatLocalDateTime(localDateTime, DATETIME_FORMATTER_SHORT);
+    }
+
+    /**
+     * 日期格式化字符串
+     *
+     * @param localDate 日期
+     * @param pattern   格式化
+     */
+    public static String formatLocalDate(LocalDate localDate, String pattern) {
+        return localDate.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    /**
+     * 时间格式化字符串
+     *
+     * @param localTime 时间
+     * @param pattern   格式化
+     */
+    public static String formatLocalTime(LocalTime localTime, String pattern) {
+        return localTime.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    /**
+     * 日期时间格式化字符串
+     *
+     * @param localDateTime 日期时间
+     * @param pattern       格式化
+     */
+    public static String formatLocalDateTime(LocalDateTime localDateTime, String pattern) {
+        return localDateTime.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    /**
+     * 日期字符串转日期
+     *
+     * @param date    日期字符串
+     * @param pattern 格式化
+     */
     public static LocalDate parseLocalDate(String date, String pattern) {
         return LocalDate.parse(date, DateTimeFormatter.ofPattern(pattern));
     }
 
+    /**
+     * 时间字符串转时间
+     *
+     * @param time    时间字符串
+     * @param pattern 格式化
+     */
     public static LocalTime parseLocalTime(String time, String pattern) {
         return LocalTime.parse(time, DateTimeFormatter.ofPattern(pattern));
     }
 
+    /**
+     * 日期时间字符串转日期时间
+     *
+     * @param dateTime 日期时间字符串
+     * @param pattern  格式化
+     */
     public static LocalDateTime parseLocalDateTime(String dateTime, String pattern) {
         return LocalDateTime.parse(dateTime, DateTimeFormatter.ofPattern(pattern));
     }
@@ -229,59 +362,87 @@ public class DateUtil {
     /**
      * 获取本周第一天
      */
-    public static String getWeekFirstDay() {
-        return LocalDate.now().minusWeeks(0).with(DayOfWeek.MONDAY).toString();
+    public static LocalDate getCurrentWeekFirstDate() {
+        return LocalDate.now().minusWeeks(0).with(DayOfWeek.MONDAY);
     }
 
     /**
      * 获取本周最后一天
      */
-    public static String getWeekLastDay() {
-        return LocalDate.now().minusWeeks(0).with(DayOfWeek.SUNDAY).toString();
+    public static LocalDate getCurrentWeekLastDate() {
+        return LocalDate.now().minusWeeks(0).with(DayOfWeek.SUNDAY);
     }
 
     /**
      * 获取本月第一天
      */
-    public static String getMonthFirstDay() {
-        return LocalDate.now().with(TemporalAdjusters.firstDayOfMonth()).toString();
+    public static LocalDate getCurrentMonthFirstDate() {
+        return LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
     }
 
     /**
      * 获取本月最后一天
      */
-    public static String getMonthLastDay() {
-        return LocalDate.now().with(TemporalAdjusters.lastDayOfMonth()).toString();
-    }
-
-    /**
-     * 获取指定月份月第一天
-     *
-     * @param date yyyy-MM-dd
-     */
-    public static String getMonthFirstDay(String date) {
-        LocalDate localDate = parseLocalDate(date);
-        return localDate.with(TemporalAdjusters.firstDayOfMonth()).toString();
+    public static LocalDate getCurrentMonthLastDate() {
+        return LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());
     }
 
     /**
      * 获取指定周第一天
      *
-     * @param date yyyy-MM-dd
+     * @param date    日期字符串
+     * @param pattern 格式化
      */
-    public static String getWeekFirstDay(String date) {
-        LocalDate localDate = parseLocalDate(date);
-        return localDate.minusWeeks(0).with(DayOfWeek.MONDAY).toString();
+    public static LocalDate getWeekFirstDate(String date, String pattern) {
+        return parseLocalDate(date, pattern).minusWeeks(0).with(DayOfWeek.MONDAY);
+    }
+
+    /**
+     * 获取指定周第一天
+     *
+     * @param localDate 日期
+     */
+    public static LocalDate getWeekFirstDate(LocalDate localDate) {
+        return localDate.minusWeeks(0).with(DayOfWeek.MONDAY);
     }
 
     /**
      * 获取指定周最后一天
      *
-     * @param date yyyy-MM-dd
+     * @param date    日期字符串
+     * @param pattern 格式化
      */
-    public static String getWeekLastDay(String date) {
-        LocalDate localDate = parseLocalDate(date);
-        return localDate.minusWeeks(0).with(DayOfWeek.SUNDAY).toString();
+    public static LocalDate getWeekLastDate(String date, String pattern) {
+        return parseLocalDate(date, pattern).minusWeeks(0).with(DayOfWeek.SUNDAY);
+    }
+
+    /**
+     * 获取指定周最后一天
+     *
+     * @param localDate 日期
+     */
+    public static LocalDate getWeekLastDate(LocalDate localDate) {
+        return localDate.minusWeeks(0).with(DayOfWeek.SUNDAY);
+    }
+
+
+    /**
+     * 获取指定月份月第一天
+     *
+     * @param date    日期字符串
+     * @param pattern 格式化
+     */
+    public static LocalDate getMonthFirstDate(String date, String pattern) {
+        return parseLocalDate(date, pattern).with(TemporalAdjusters.firstDayOfMonth());
+    }
+
+    /**
+     * 获取指定月份月第一天
+     *
+     * @param localDate
+     */
+    public static LocalDate getMonthFirstDate(LocalDate localDate) {
+        return localDate.with(TemporalAdjusters.firstDayOfMonth());
     }
 
     /**
@@ -293,79 +454,105 @@ public class DateUtil {
         return LocalDate.now().getDayOfWeek().getValue();
     }
 
-    public static int getCurrentWeek(String date) {
-        return parseLocalDate(date).getDayOfWeek().getValue();
+
+    /**
+     * 获取当前星期
+     *
+     * @param localDate 日期
+     */
+    public static int getWeek(LocalDate localDate) {
+        return localDate.getDayOfWeek().getValue();
     }
 
-    public static int getCurrentWeek(String date, String pattern) {
+    /**
+     * 获取指定日期的星期
+     *
+     * @param date    日期字符串
+     * @param pattern 格式化
+     */
+    public static int getWeek(String date, String pattern) {
         return parseLocalDate(date, pattern).getDayOfWeek().getValue();
     }
 
     /**
      * 日期相隔天数
      *
-     * @param startDateInclusive
-     * @param endDateExclusive
+     * @param startLocalDate 起日期
+     * @param endLocalDate   止日期
      */
-    public static long periodDays(LocalDate startDateInclusive, LocalDate endDateExclusive) {
-        return endDateExclusive.toEpochDay() - startDateInclusive.toEpochDay();
+    public static long intervalDays(LocalDate startLocalDate, LocalDate endLocalDate) {
+        return endLocalDate.toEpochDay() - startLocalDate.toEpochDay();
     }
 
     /**
      * 日期相隔小时
      *
-     * @param startInclusive
-     * @param endExclusive
+     * @param startLocalDateTime 起日期时间
+     * @param endLocalDateTime   止日期时间
      */
-    public static long durationHours(Temporal startInclusive, Temporal endExclusive) {
-        return Duration.between(startInclusive, endExclusive).toHours();
+    public static long intervalHours(LocalDateTime startLocalDateTime, LocalDateTime endLocalDateTime) {
+        return Duration.between(startLocalDateTime, endLocalDateTime).toHours();
     }
 
     /**
      * 日期相隔分钟
      *
-     * @param startInclusive
-     * @param endExclusive
+     * @param startLocalDateTime 起日期时间
+     * @param endLocalDateTime   止日期时间
      */
-    public static long durationMinutes(Temporal startInclusive, Temporal endExclusive) {
-        return Duration.between(startInclusive, endExclusive).toMinutes();
+    public static long intervalMinutes(LocalDateTime startLocalDateTime, LocalDateTime endLocalDateTime) {
+        return Duration.between(startLocalDateTime, endLocalDateTime).toMinutes();
     }
 
     /**
      * 日期相隔毫秒数
      *
-     * @param startInclusive
-     * @param endExclusive
+     * @param startLocalDateTime 起日期时间
+     * @param endLocalDateTime   止日期时间
      */
-    public static long durationMillis(Temporal startInclusive, Temporal endExclusive) {
-        return Duration.between(startInclusive, endExclusive).toMillis();
+    public static long intervalMillis(LocalDateTime startLocalDateTime, LocalDateTime endLocalDateTime) {
+        return Duration.between(startLocalDateTime, endLocalDateTime).toMillis();
+    }
+
+    /**
+     * 当前是否闰年
+     */
+    public static boolean isCurrentLeapYear() {
+        return LocalDate.now().isLeapYear();
+    }
+
+    /**
+     * 是否闰年
+     */
+    public static boolean isLeapYear(LocalDate localDate) {
+        return localDate.isLeapYear();
     }
 
     /**
      * 是否当天
      *
-     * @param date
+     * @param localDate 日期
      */
-    public static boolean isToday(LocalDate date) {
-        return LocalDate.now().equals(date);
+    public static boolean isToday(LocalDate localDate) {
+        return LocalDate.now().equals(localDate);
     }
 
     /**
      * 获取此日期时间与默认时区<Asia/Shanghai>组合的时间毫秒数
      *
-     * @param dateTime
+     * @param localDateTime 日期时间
      */
-    public static Long toEpochMilli(LocalDateTime dateTime) {
-        return dateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    public static Long toEpochMilli(LocalDateTime localDateTime) {
+        return localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
     }
 
     /**
      * 获取此日期时间与指定时区组合的时间毫秒数
      *
-     * @param dateTime
+     * @param localDateTime 日期时间
      */
-    public static Long toSelectEpochMilli(LocalDateTime dateTime, ZoneId zoneId) {
-        return dateTime.atZone(zoneId).toInstant().toEpochMilli();
+    public static Long toSelectEpochMilli(LocalDateTime localDateTime, ZoneId zoneId) {
+        return localDateTime.atZone(zoneId).toInstant().toEpochMilli();
     }
 
 }
