c2c00681608bf7cd67d7a9921c881a03fd28be7f
Yanzheng <micyo202@163.com>
2020-05-22 11:12:21 +0800

    更新全局异常处理类

--- lion-common/src/main/java/com/lion/common/exception/GlobalExceptionHandler.java
+++ lion-common/src/main/java/com/lion/common/exception/GlobalExceptionHandler.java
@@ -25,15 +25,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
- * LionExceptionHandler
- * 自定义全局异常处理类
+ * GlobalExceptionHandler
+ * 全局异常处理类
  *
  * @author Yanzheng (https://github.com/micyo202)
  * @date 2019/07/17
  */
 @RestControllerAdvice
 @Slf4j
-public class LionExceptionHandler {
+public class GlobalExceptionHandler {
 
     /**
      * 声明要捕获的异常
