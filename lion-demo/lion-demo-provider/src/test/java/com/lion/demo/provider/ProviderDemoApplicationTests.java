7d248af700d4dac12c14ac9f10cf317a81876c13
Yanzheng <micyo202@163.com>
2020-05-24 12:17:21 +0800

    增加provider单元测试类mock服务

---
+++ lion-demo/lion-demo-provider/src/test/java/com/lion/demo/provider/ProviderDemoApplicationTests.java
@@ -0,0 +1,104 @@
+/**
+ *   Copyright 2019 Yanzheng (https://github.com/micyo202). All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.lion.demo.provider;
+
+import com.lion.common.result.Result;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+/**
+ * ProviderDemoApplicationTests
+ * 单元测试，使用 mock 需将 druid 配置中 web-stat-filter.enabled 设为关闭
+ *
+ * @author Yanzheng (https://github.com/micyo202)
+ * @date 2020/5/20
+ */
+//@SpringBootTest(classes = ProviderDemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureMockMvc
+public class ProviderDemoApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    private String getUrl() {
+        return "http://localhost:" + port;
+    }
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void restTestInit() {
+        String url = getUrl();
+        ResponseEntity<Result> response = this.restTemplate.getForEntity(
+                url + "/init", Result.class);
+        System.out.println(response.getBody());
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void mockTestInit() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders.get("/init")
+                        .accept(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+        System.out.println(mvcResult);
+    }
+
+    @Test
+    public void mockTestHi() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders.get("/hi")
+                        .param("name", "lion")
+                        .accept(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+        System.out.println(mvcResult);
+    }
+
+    @Test
+    public void mockTestTempProductDeduct() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders.get("/deduct")
+                        .param("productCode", "product-1")
+                        .param("count", "2")
+                        .accept(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+        System.out.println(mvcResult);
+    }
+
+}
